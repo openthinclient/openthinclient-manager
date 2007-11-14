03fb78557ab8f5c08804a2edf26689b2def8126b
Joerg Henne <j.henne@levigo.de>
2007-11-14 13:55:06 +0000

    Cleanup

--- common/src/main/java/org/openthinclient/common/model/User.java
+++ common/src/main/java/org/openthinclient/common/model/User.java
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place - Suite 330, Boston, MA 02111-1307, USA.
- *******************************************************************************/
+ ******************************************************************************/
 package org.openthinclient.common.model;
 
 import java.security.MessageDigest;
@@ -32,187 +32,178 @@ import org.castor.util.Base64Encoder;
  * @author levigo
  */
 public class User extends DirectoryObject implements AssociatedObjectsProvider {
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  private Set<UserGroup> userGroups;
+	private Set<UserGroup> userGroups;
 
-  private Set<ApplicationGroup> applicationGroups;
-  private Set<Application> applications;
-  private Set<Printer> printers;
+	private Set<ApplicationGroup> applicationGroups;
+	private Set<Application> applications;
+	private Set<Printer> printers;
 
-  private Location location;
-  private String stringLocation;
+	private Location location;
 
-  private String sn;
-  private String givenName;
-  private byte[] userPassword;
-  private String newPassword = "";
-  private String verifyPassword = "";
+	private String sn;
+	private String givenName;
+	private byte[] userPassword;
+	private String newPassword = "";
+	private String verifyPassword = "";
 
-  private Integer uid;
-  
-  public Set<ApplicationGroup> getApplicationGroups() {
-    return applicationGroups;
-  }
+	private Integer uid;
 
-  public void setApplicationGroups(Set<ApplicationGroup> applicationGroups) {
-    this.applicationGroups = applicationGroups;
-    firePropertyChange("applicationGroups", null, applicationGroups);
-  }
+	public Set<ApplicationGroup> getApplicationGroups() {
+		return applicationGroups;
+	}
 
-  public Set<Application> getApplications() {
-    return applications;
-  }
+	public void setApplicationGroups(Set<ApplicationGroup> applicationGroups) {
+		this.applicationGroups = applicationGroups;
+		firePropertyChange("applicationGroups", null, applicationGroups);
+	}
 
-  public void setApplications(Set<Application> applications) {
-    this.applications = applications;
-    firePropertyChange("applications", null, applications);
-  }
+	public Set<Application> getApplications() {
+		return applications;
+	}
 
-//  public Location getLocation() {
-//    return location;
-//  }
-//  
-//  public String getStringLocation() {
-//	    return stringLocation;
-//  }
+	public void setApplications(Set<Application> applications) {
+		this.applications = applications;
+		firePropertyChange("applications", null, applications);
+	}
 
-  public void setLocation(Location location) {
-    this.location = location;
-    if(location == null) {
-    	this.stringLocation = "";
-    }else {
-    	this.stringLocation = location.toString();
-    } 
-    firePropertyChange("location", null, location);
-  }
+	public void setLocation(Location location) {
+		this.location = location;
 
-  public Set<Printer> getPrinters() {
-    return printers;
-  }
+		firePropertyChange("location", null, location);
+	}
 
-  public void setPrinters(Set<Printer> printers) {
-    this.printers = printers;
-    firePropertyChange("printers", null, printers);
-  }
+	public Set<Printer> getPrinters() {
+		return printers;
+	}
 
-  public Set<UserGroup> getUserGroups() {
-    return userGroups;
-  }
+	public void setPrinters(Set<Printer> printers) {
+		this.printers = printers;
+		firePropertyChange("printers", null, printers);
+	}
 
-  public void setUserGroups(Set<UserGroup> userGroups) {
-    this.userGroups = userGroups;
-    firePropertyChange("userGroups", null, userGroups);
-  }
+	public Set<UserGroup> getUserGroups() {
+		return userGroups;
+	}
 
-  public String getGivenName() {
-    return givenName;
-  }
+	public void setUserGroups(Set<UserGroup> userGroups) {
+		this.userGroups = userGroups;
+		firePropertyChange("userGroups", null, userGroups);
+	}
 
-  public void setGivenName(String givenName) {
-    this.givenName = givenName;
-    firePropertyChange("givenName", null, givenName);
-  }
+	public String getGivenName() {
+		return givenName;
+	}
 
-  public String getSn() {
-  	if(null == sn)
-  		return this.getName();
-    return sn;
-  }
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+		firePropertyChange("givenName", null, givenName);
+	}
 
-  public void setSn(String sn) {
-    this.sn = sn;
-    firePropertyChange("sn", null, sn);
-  }
+	public String getSn() {
+		if (null == sn)
+			return this.getName();
+		return sn;
+	}
 
-  /*
-   * @see org.openthinclient.common.model.AssociatedObjectsProvider#getAssociatedObjects()
-   */
-  public Map<Class, Set<? extends DirectoryObject>> getAssociatedObjects() {
-    Map<Class, Set<? extends DirectoryObject>> assocObjects = new HashMap<Class, Set<? extends DirectoryObject>>();
-    assocObjects.put(Application.class, applications);
-    assocObjects.put(ApplicationGroup.class, applicationGroups);
-    assocObjects.put(Printer.class, printers);
-    assocObjects.put(UserGroup.class, userGroups);
+	public void setSn(String sn) {
+		this.sn = sn;
+		firePropertyChange("sn", null, sn);
+	}
 
-    return assocObjects;
-  }
+	/*
+	 * @see org.openthinclient.common.model.AssociatedObjectsProvider#getAssociatedObjects()
+	 */
+	public Map<Class, Set<? extends DirectoryObject>> getAssociatedObjects() {
+		Map<Class, Set<? extends DirectoryObject>> assocObjects = new HashMap<Class, Set<? extends DirectoryObject>>();
+		assocObjects.put(Application.class, applications);
+		assocObjects.put(ApplicationGroup.class, applicationGroups);
+		assocObjects.put(Printer.class, printers);
+		assocObjects.put(UserGroup.class, userGroups);
 
-  /*
-   * @see org.openthinclient.common.model.AssociatedObjectsProvider#setAssociatedObjects(java.lang.Class,
-   *      java.util.Set)
-   */
-  public void setAssociatedObjects(Class subgroupClass,
-      Set<? extends DirectoryObject> subgroups) {
-    if (subgroupClass.equals(Application.class)) {
-      setApplications((Set<Application>) subgroups);
-    }
-    if (subgroupClass.equals(ApplicationGroup.class)) {
-      setApplicationGroups((Set<ApplicationGroup>) subgroups);
-    }
-    if (subgroupClass.equals(Printer.class)) {
-      setPrinters((Set<Printer>) subgroups);
-    }
+		return assocObjects;
+	}
 
-    if (subgroupClass.equals(UserGroup.class)) {
-    		setUserGroups((Set<UserGroup>) subgroups);
-    }
-  }
+	/*
+	 * @see org.openthinclient.common.model.AssociatedObjectsProvider#setAssociatedObjects(java.lang.Class,
+	 *      java.util.Set)
+	 */
+	public void setAssociatedObjects(Class subgroupClass,
+			Set<? extends DirectoryObject> subgroups) {
+		if (subgroupClass.equals(Application.class)) {
+			setApplications((Set<Application>) subgroups);
+		}
+		if (subgroupClass.equals(ApplicationGroup.class)) {
+			setApplicationGroups((Set<ApplicationGroup>) subgroups);
+		}
+		if (subgroupClass.equals(Printer.class)) {
+			setPrinters((Set<Printer>) subgroups);
+		}
 
-  /**
-   * @return
-   * @deprecared Used for LDAP-Mapping only
-   */
-  public byte[] getUserPassword() {
-    return userPassword;
-  }
+		if (subgroupClass.equals(UserGroup.class)) {
+			setUserGroups((Set<UserGroup>) subgroups);
+		}
+	}
 
-  /**
-   * @param userPassword
-   * @deprecared Used for LDAP-Mapping only
-   */
-  public void setUserPassword(byte[] userPassword) {
-    this.userPassword = userPassword;
-  }
+	/**
+	 * @return
+	 * @deprecared Used for LDAP-Mapping only
+	 */
+	public byte[] getUserPassword() {
+		return userPassword;
+	}
 
-  public String getNewPassword() {
-    // we never hand out the real password, only the change one for verification
-    return newPassword;
-  }
+	/**
+	 * @param userPassword
+	 * @deprecared Used for LDAP-Mapping only
+	 */
+	public void setUserPassword(byte[] userPassword) {
+		this.userPassword = userPassword;
+	}
 
-  public void setNewPassword(String password) {
-    try {
-      MessageDigest digest = MessageDigest.getInstance("MD5");
-      digest.update(password.getBytes());
-      String encrypted = "{MD5}"
-          + new String(Base64Encoder.encode(digest.digest()));
+	public String getNewPassword() {
+		// we never hand out the real password, only the change one for verification
+		return newPassword;
+	}
 
-      setUserPassword(encrypted.getBytes());
-//      setUserPassword(password.getBytes());
+	public void setNewPassword(String password) {
+		try {
+			MessageDigest digest = MessageDigest.getInstance("MD5");
+			digest.update(password.getBytes());
+			String encrypted = "{MD5}"
+					+ new String(Base64Encoder.encode(digest.digest()));
 
-      this.newPassword = password;
+			setUserPassword(encrypted.getBytes());
+			// setUserPassword(password.getBytes());
 
-      firePropertyChange("newPassword", "", password);
-      firePropertyChange("password", new byte[0], getUserPassword());
-    } catch (NoSuchAlgorithmException e) {
-      throw new RuntimeException("Can't encrypt user's password", e);
-    }
-  }
+			this.newPassword = password;
 
-  public String getVerifyPassword() {
-    return verifyPassword;
-  }
+			firePropertyChange("newPassword", "", password);
+			firePropertyChange("password", new byte[0], getUserPassword());
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("Can't encrypt user's password", e);
+		}
+	}
 
-  public void setVerifyPassword(String verifyPassword) {
-    this.verifyPassword = verifyPassword;
-    firePropertyChange("verifyPassword", "", verifyPassword);
-  }
+	public String getVerifyPassword() {
+		return verifyPassword;
+	}
 
-  public Integer getUid() {
-    return uid;
-  }
+	public void setVerifyPassword(String verifyPassword) {
+		this.verifyPassword = verifyPassword;
+		firePropertyChange("verifyPassword", "", verifyPassword);
+	}
 
-  public void setUid(Integer uid) {
-    this.uid = uid;
-  }
+	public Integer getUid() {
+		return uid;
+	}
+
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
 }
