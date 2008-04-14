324f7d86aca668d9ab3e54ee240902d6a7e05f68
Martin Kreiner <m.kreiner@openthinclient.com>
2008-04-14 19:03:54 +0000

    CLOSES SUITE-161: Eliminate ConcurrentModificationExceptions in nfs server's cache cleaner https://issues.openthinclient.org/otc/browse/SUITE-161

--- server/nfs-service/src/main/java/org/openthinclient/nfsd/CacheCleaner.java
+++ server/nfs-service/src/main/java/org/openthinclient/nfsd/CacheCleaner.java
@@ -28,13 +28,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.ConcurrentModificationException;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 
@@ -44,129 +44,153 @@ import org.apache.log4j.Logger;
 public class CacheCleaner {
 	private static final Logger logger = Logger.getLogger(CacheCleaner.class);
 
-	private static Set<NFSFile> dirtyFilesSet = new HashSet<NFSFile>();
-	private static Set<NFSFile> openFilesSet = new HashSet<NFSFile>();
+	private static class Janitor implements Runnable {
+		private final BlockingQueue<NFSFile> taintQueue;
+		private final Set<NFSFile> dirtyFilesSet = new HashSet<NFSFile>();
+		private final Set<NFSFile> openFilesSet = new HashSet<NFSFile>();
+		private boolean shutdownRequested;
 
-	private static class ExpiryTask extends TimerTask {
+		public Janitor(BlockingQueue<NFSFile> taintQueue) {
+			this.taintQueue = taintQueue;
+
+			final Thread t = new Thread(this, "CacheCleaner");
+			t.setDaemon(true);
+			t.setPriority(Thread.NORM_PRIORITY - 1);
+			t.start();
+		}
+
+		private static long EXPIRY_INTERVAL = 5000;
+
+		private static final int MAX_OPEN_FILES = 100;
+
 		@Override
 		public void run() {
-			logger.debug("Running expiry");
-			try {
-				synchronized (this) {
-					for (final Iterator<NFSFile> i = dirtyFilesSet.iterator(); i
-							.hasNext();) {
-						final NFSFile file = i.next();
-						synchronized (file) {
-							if (file.getLastAccessTimestamp()
-									+ file.getExport().getCacheTimeout() < System
-									.currentTimeMillis())
-								try {
-									if (logger.isDebugEnabled())
-										logger.debug("Flushing cache for " + file.getFile());
+			final long lastExpiry = 0;
 
-									file.flushCache();
-
-									openFilesSet.remove(file);
-
-									i.remove();
-								} catch (final IOException e) {
-									logger.warn("Got exception flushing cache for "
-											+ file.getFile());
-								}
+			while (!shutdownRequested) {
+				final long timeTillNextExpiry = Math.max(0, System.currentTimeMillis()
+						- lastExpiry - EXPIRY_INTERVAL);
+				try {
+					final NFSFile file = taintQueue.poll(timeTillNextExpiry,
+							TimeUnit.MILLISECONDS);
+					if (null != file) {
+						dirtyFilesSet.add(file);
+						if (file.isChannelOpen()) {
+							openFilesSet.add(file);
+							if (openFilesSet.size() >= MAX_OPEN_FILES)
+								forceCacheFlush();
 						}
-					}
+					} else
+						// no file - it must be time for an expiry
+						expire();
+				} catch (final InterruptedException e) {
+					// ignore
 				}
-
-				// we don't synchronize everything. therefore the estimate is just that:
-				// an estimate. Therefore we bring down the count of open files to
-				// zero, when there is nothing cached.
-				if (dirtyFilesSet.size() == 0 && openFilesSet.size() != 0)
-					openFilesSet.clear();
-
-			} catch (final ConcurrentModificationException e) {
-				// ignore!
 			}
+		}
+
+		private synchronized void expire() {
+			logger.debug("Running expiry");
+			for (final Iterator<NFSFile> i = dirtyFilesSet.iterator(); i.hasNext();) {
+				final NFSFile file = i.next();
+				synchronized (file) {
+					if (file.getLastAccessTimestamp()
+							+ file.getExport().getCacheTimeout() < System.currentTimeMillis())
+						try {
+							if (logger.isDebugEnabled())
+								logger.debug("Flushing cache for " + file.getFile());
+
+							file.flushCache();
+
+							openFilesSet.remove(file);
+
+							i.remove();
+						} catch (final IOException e) {
+							logger.warn("Got exception flushing cache for " + file.getFile());
+						}
+				}
+			}
+
+			// we don't synchronize everything. therefore the estimate is just
+			// that:
+			// an estimate. Therefore we bring down the count of open files to
+			// zero, when there is nothing cached.
+			if (dirtyFilesSet.size() == 0 && openFilesSet.size() != 0)
+				openFilesSet.clear();
 
 			if (logger.isDebugEnabled())
 				logger.debug("Expiry done. Dirty files remaining: "
 						+ dirtyFilesSet.size() + " open files remaining: "
 						+ openFilesSet.size());
 		}
-	}
 
-	private static Timer expiryTimer;
-
-	private static long EXPIRY_INTERVAL = 5000;
-
-	private static final int MAX_OPEN_FILES = 100;
-
-	static {
-		expiryTimer = new Timer();
-		expiryTimer.scheduleAtFixedRate(new ExpiryTask(), EXPIRY_INTERVAL,
-				EXPIRY_INTERVAL);
-	}
-
-	public synchronized static void registerDirtyFile(NFSFile file) {
-		dirtyFilesSet.add(file);
-		if (file.isChannelOpen()) {
-			openFilesSet.add(file);
-			if (openFilesSet.size() >= MAX_OPEN_FILES)
-				forceCacheFlush();
-		}
-	}
-
-	private synchronized static void forceCacheFlush() {
-		logger.info("Number of open files: " + openFilesSet.size()
-				+ ". Forcing flush of cache.");
-		final List<NFSFile> sortedList = new ArrayList<NFSFile>(dirtyFilesSet);
-		Collections.sort(sortedList, new Comparator<NFSFile>() {
-			public int compare(NFSFile f1, NFSFile f2) {
-				// implement reverse order!
-				// we assume that the difference can't ever exceed Integer.MAX_VALUE;
-				return (int) (f1.getLastAccessTimestamp() - f2.getLastAccessTimestamp());
-			}
-		});
-
-		final int lowWaterMark = MAX_OPEN_FILES * 2 / 3;
-		for (final Iterator<NFSFile> i = sortedList.iterator(); i.hasNext();) {
-			final NFSFile file = i.next();
-
-			synchronized (file) {
-				try {
-					if (logger.isDebugEnabled())
-						logger.debug("Flushing cache for file of age "
-								+ (System.currentTimeMillis() - file.getLastAccessTimestamp()));
-
-					file.flushCache();
-					openFilesSet.remove(file);
-					dirtyFilesSet.remove(file);
-				} catch (final IOException e) {
-					logger.warn("Got exception flushing cache for " + file.getFile());
+		private synchronized void forceCacheFlush() {
+			logger.info("Number of open files: " + openFilesSet.size()
+					+ ". Forcing flush of cache.");
+			final List<NFSFile> sortedList = new ArrayList<NFSFile>(dirtyFilesSet);
+			Collections.sort(sortedList, new Comparator<NFSFile>() {
+				public int compare(NFSFile f1, NFSFile f2) {
+					// implement reverse order!
+					// we assume that the difference can't ever exceed Integer.MAX_VALUE;
+					return (int) (f1.getLastAccessTimestamp() - f2
+							.getLastAccessTimestamp());
 				}
+			});
+
+			final int lowWaterMark = MAX_OPEN_FILES * 2 / 3;
+			for (final Iterator<NFSFile> i = sortedList.iterator(); i.hasNext();) {
+				final NFSFile file = i.next();
+
+				synchronized (file) {
+					try {
+						if (logger.isDebugEnabled())
+							logger
+									.debug("Flushing cache for file of age "
+											+ (System.currentTimeMillis() - file
+													.getLastAccessTimestamp()));
+
+						file.flushCache();
+						openFilesSet.remove(file);
+						dirtyFilesSet.remove(file);
+					} catch (final IOException e) {
+						logger.warn("Got exception flushing cache for " + file.getFile());
+					}
+				}
+
+				// cleanup up to low water mark.
+				if (openFilesSet.size() < lowWaterMark)
+					break;
 			}
-
-			// cleanup up to low water mark.
-			if (openFilesSet.size() < lowWaterMark)
-				break;
 		}
-	}
 
-	public synchronized static void flushAll() {
-		if (logger.isDebugEnabled())
-			logger.debug("Flushing cache for all " + openFilesSet.size()
-					+ " open files.");
+		public synchronized void flushAll() {
+			if (logger.isDebugEnabled())
+				logger.debug("Flushing cache for all " + openFilesSet.size()
+						+ " open files.");
 
-		for (final Iterator i = openFilesSet.iterator(); i.hasNext();) {
-			final NFSFile file = (NFSFile) i.next();
-			synchronized (file) {
-				try {
-					file.flushCache();
-					i.remove();
-					dirtyFilesSet.remove(file);
-				} catch (final IOException e) {
-					logger.warn("Got exception flushing cache for " + file.getFile());
+			for (final Iterator i = openFilesSet.iterator(); i.hasNext();) {
+				final NFSFile file = (NFSFile) i.next();
+				synchronized (file) {
+					try {
+						file.flushCache();
+						i.remove();
+						dirtyFilesSet.remove(file);
+					} catch (final IOException e) {
+						logger.warn("Got exception flushing cache for " + file.getFile());
+					}
 				}
 			}
 		}
+	}
+
+	private static final BlockingQueue<NFSFile> taintQueue = new LinkedBlockingQueue<NFSFile>();
+	private static final Janitor janitor = new Janitor(taintQueue);
+
+	public static void registerDirtyFile(NFSFile file) {
+		taintQueue.offer(file);
+	}
+
+	public static void flushAll() {
+		janitor.flushAll();
 	}
 }
