a64695df885156b7c3decb66378d72ce92e81d04
Francois Fernandes <f.fernandes@fernandes-it.de>
2015-02-11 08:46:58 +0100

    Commenting the implementation temporarily out

--- services/dhcp/src/main/java/org/openthinclient/service/dhcp/RemotedBean.java
+++ services/dhcp/src/main/java/org/openthinclient/service/dhcp/RemotedBean.java
@@ -18,33 +18,29 @@
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place - Suite 330, Boston, MA 02111-1307, USA.
  ******************************************************************************/
-package org.openthinclient.remoted;
+package org.openthinclient.service.dhcp;
 
-import javax.ejb.Remote;
-import javax.ejb.Stateless;
-import javax.management.MBeanServer;
-import javax.management.MBeanServerFactory;
-import javax.management.ObjectName;
 
-import org.apache.log4j.Logger;
-import org.jboss.annotation.ejb.RemoteBinding;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Stateless
-@RemoteBinding(jndiBinding = "RemotedBean/remote")
-@Remote(RemotedBean.class)
 public class RemotedBean implements Remoted {
-	private static final Logger logger = Logger.getLogger(RemotedBean.class);
+	private static final Logger logger = LoggerFactory.getLogger(RemotedBean.class);
 
 	public boolean dhcpReloadRealms() throws Exception {
-		final ObjectName objectName = new ObjectName("tcat:service=ConfigService");
-		final MBeanServer server = MBeanServerFactory.findMBeanServer(null).get(0);
+//		final ObjectName objectName = new ObjectName("tcat:service=ConfigService");
+//		final MBeanServer server = MBeanServerFactory.findMBeanServer(null).get(0);
+//
+//		if (Boolean.FALSE.equals(server.invoke(objectName, "reloadRealms",
+//				new Object[]{}, new String[]{}))) {
+//			logger.error("Unable to reloadRealms");
+//			return false;
+//		} else
+//			return true;
 
-		if (Boolean.FALSE.equals(server.invoke(objectName, "reloadRealms",
-				new Object[]{}, new String[]{}))) {
-			logger.error("Unable to reloadRealms");
-			return false;
-		} else
-			return true;
+    // FIXME this implementation has to be adapted to the new service control mechanism
 
-	}
+    throw new UnsupportedOperationException();
+
+  }
 }
