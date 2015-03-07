2f7f7fe9ee88d723ec6786e2fb5dd117953f6d26
Francois Fernandes <f.fernandes@fernandes-it.de>
2015-03-07 14:05:46 +0100

    corrected the unit test

--- services/package-manager/src/test/java/org/openthinclient/pkgmgr/SimpleTestingPackageManagerConfiguration.java
+++ services/package-manager/src/test/java/org/openthinclient/pkgmgr/SimpleTestingPackageManagerConfiguration.java
@@ -1,7 +1,10 @@
 package org.openthinclient.pkgmgr;
 
 import org.openthinclient.pkgmgr.impl.PackageManagerImpl;
+import org.openthinclient.service.nfs.NFS;
 import org.openthinclient.util.dpkg.DPKGPackageManager;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,7 +18,8 @@ public class SimpleTestingPackageManagerConfiguration {
 
   @Bean(destroyMethod = "close")
   public PackageManagerImpl packageManagerImpl(DPKGPackageManager dpkgPackageManager) {
-    return new PackageManagerImpl(dpkgPackageManager);
+    // FIXME is there a way to have the nfs server optionally passed?
+    return new PackageManagerImpl(dpkgPackageManager, null);
   }
 
 }
