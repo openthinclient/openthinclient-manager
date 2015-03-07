30f5bcaa06cdfb0956f21949ea25801f4c600768
Francois Fernandes <f.fernandes@fernandes-it.de>
2015-03-07 01:07:32 +0100

    recover accident

--- services/common/src/main/java/org/openthinclient/service/common/Service.java
+++ services/common/src/main/java/org/openthinclient/service/common/Service.java
@@ -3,7 +3,6 @@ package org.openthinclient.service.common;
 import org.openthinclient.service.common.home.Configuration;
 
 public interface Service<CONF extends Configuration> {
-public interface Service<CONF extends Configuration> {
 
   void setConfiguration(CONF configuration);
 
