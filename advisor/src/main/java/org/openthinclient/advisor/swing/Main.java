7cb74dc81772f6710965171e2f16ee6d7e61a85d
Francois Fernandes <f.fernandes@fernandes-it.de>
2015-05-30 13:38:16 +0200

    UI init on the EDT

--- advisor/src/main/java/org/openthinclient/advisor/swing/Main.java
+++ advisor/src/main/java/org/openthinclient/advisor/swing/Main.java
@@ -1,18 +1,24 @@
 package org.openthinclient.advisor.swing;
 
+import javax.swing.SwingUtilities;
+
 /**
  * Main-Klasse die beim starten des Programms ausgef??hrt wird. Sie erzeugt ein
  * Objekt des jFrames Startscreen und macht diesen sichtbar.
+ *
  * @author Benedikt Diehl
  */
 public class Main {
 
-    /**
-     *
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Startscreen startscreen = new Startscreen();
-        startscreen.setVisible(true);
-    }
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+
+    SwingUtilities.invokeLater(() -> {
+      Startscreen startscreen = new Startscreen();
+      startscreen.setVisible(true);
+    });
+
+  }
 }
