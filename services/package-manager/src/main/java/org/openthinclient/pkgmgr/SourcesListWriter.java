7790776dfc87e8300c4b4febff8664959a2ac728
Francois Fernandes <f.fernandes@fernandes-it.de>
2015-08-11 12:20:39 +0200

    More flexible SourcesListWriter API and unit tests

--- services/package-manager/src/main/java/org/openthinclient/pkgmgr/SourcesListWriter.java
+++ services/package-manager/src/main/java/org/openthinclient/pkgmgr/SourcesListWriter.java
@@ -2,50 +2,94 @@ package org.openthinclient.pkgmgr;
 
 import com.google.common.base.Strings;
 
+import java.io.IOException;
 import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class SourcesListWriter {
 
-  public void write(SourcesList sourcesList, OutputStream out) {
+  private final String NL = "\n";
 
-    final PrintWriter printStream = new PrintWriter(out);
+  public void write(SourcesList sourcesList, OutputStream out) throws SourcesListWriterException {
+
+    try {
+      final OutputStreamWriter w = new OutputStreamWriter(out, "UTF-8");
+      write(sourcesList, w);
+      w.flush();
+    } catch (IOException e) {
+      throw new SourcesListWriterException("sources list writing failed", e);
+    }
+
+  }
+
+  public void write(SourcesList sourcesList, Writer writer) throws SourcesListWriterException {
 
     sourcesList.getSources().forEach(source -> {
 
-      if (!Strings.isNullOrEmpty(source.getDescription())) {
-        final String[] lines = source.getDescription().split("\\r?\\n");
-        if (lines.length > 0) {
-          for (String line : lines) {
-            printStream.print("# ");
-            printStream.print(line);
-            printStream.println();
+      try {
+        if (!Strings.isNullOrEmpty(source.getDescription())) {
+          final String[] lines = source.getDescription().split("\\r?\\n");
+          if (lines.length > 0) {
+            for (String line : lines) {
+              writer.write("# ");
+              writer.write(line);
+              writer.write(NL);
+            }
           }
         }
-      }
 
-      if (!source.isEnabled()) {
-        printStream.print("#");
-      }
+        if (!source.isEnabled()) {
+          writer.write("#");
+        }
 
-      if (source.getType() == Source.Type.PACKAGE) {
-        printStream.print(SourcesListParser.DEB);
-      } else {
-        printStream.print(SourcesListParser.DEB_SRC);
-      }
+        if (source.getType() == Source.Type.PACKAGE) {
+          writer.write(SourcesListParser.DEB);
+        } else {
+          writer.write(SourcesListParser.DEB_SRC);
+        }
 
-      printStream.print(" ");
-      printStream.print(source.getUrl().toExternalForm());
-      printStream.print(" ");
-      printStream.print(source.getDistribution());
-      if (source.getComponents() != null && source.getComponents().size() > 0) {
-        source.getComponents().forEach(comp -> {
-          printStream.print(" ");
-          printStream.print(comp);
-        });
+        writer.write(" ");
+        writer.write(source.getUrl().toExternalForm());
+        writer.write(" ");
+        writer.write(source.getDistribution());
+        if (source.getComponents() != null && source.getComponents().size() > 0) {
+          source.getComponents().forEach(comp -> {
+            try {
+              writer.write(" ");
+              writer.write(comp);
+            } catch (IOException e) {
+              throw new SourcesListWriterException("sources list writing failed", e);
+            }
+          });
+        }
+        writer.write(NL);
+      } catch (IOException e) {
+        throw new SourcesListWriterException("sources list writing failed", e);
       }
     });
 
-    printStream.flush();
+    try {
+      writer.flush();
+    } catch (IOException e) {
+      throw new SourcesListWriterException("sources list writing failed", e);
+    }
+  }
+
+  public static class SourcesListWriterException extends RuntimeException {
+    public SourcesListWriterException() {
+    }
+
+    public SourcesListWriterException(String message) {
+      super(message);
+    }
+
+    public SourcesListWriterException(String message, Throwable cause) {
+      super(message, cause);
+    }
+
+    public SourcesListWriterException(Throwable cause) {
+      super(cause);
+    }
   }
 }
--- services/package-manager/src/test/java/org/openthinclient/pkgmgr/SourcesListWriterTest.java
+++ services/package-manager/src/test/java/org/openthinclient/pkgmgr/SourcesListWriterTest.java
@@ -1,6 +1,90 @@
 package org.openthinclient.pkgmgr;
 
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static junit.framework.Assert.assertEquals;
+
 public class SourcesListWriterTest {
+  @Test
+  public void testWriteSingleSourceNoComment() throws Exception {
 
+    final SourcesList sl = new SourcesList();
+    sl.getSources().add(createEnabledSource("http://some.host/url", "dist"));
 
+    writeAndCompare("deb http://some.host/url dist\n", sl);
+
+  }
+
+  @Test
+  public void testWriteSingleDisabledSourceNoComment() throws Exception {
+    final SourcesList sl = new SourcesList();
+    sl.getSources().add(createDisabledSource("http://some.host/url", "dist"));
+
+    writeAndCompare("#deb http://some.host/url dist\n", sl);
+
+  }
+
+  @Test
+  public void testWriteMultipleSourcesNoComment() throws Exception {
+    final SourcesList sl = new SourcesList();
+
+    sl.getSources().add(createEnabledSource("http://some.host/url", "dist"));
+    sl.getSources().add(createDisabledSource("http://some.host/url", "./"));
+    sl.getSources().add(createEnabledSource("http://another.host/path", "openthinclient"));
+
+    writeAndCompare("deb http://some.host/url dist\n" + //
+            "#deb http://some.host/url ./\n" + //
+            "deb http://another.host/path openthinclient\n", sl);
+  }
+
+  @Test
+  public void testWriteSingleSourceWithComment() throws Exception {
+    final SourcesList sl = new SourcesList();
+    sl.getSources().add(createEnabledSource("http://some.host/url", "dist", "This is a comment\nspanning multiple lines"));
+
+    writeAndCompare("# This is a comment\n" +
+            "# spanning multiple lines\n" +
+            "deb http://some.host/url dist\n", sl);
+
+  }
+
+  private void writeAndCompare(String expected, SourcesList sl) {
+    final SourcesListWriter w = new SourcesListWriter();
+    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    w.write(sl, baos);
+
+    assertEquals(expected, new String(baos.toByteArray()));
+  }
+
+  private Source createEnabledSource(String url, String distribution) throws MalformedURLException {
+    return createEnabledSource(url, distribution, null);
+  }
+
+  private Source createEnabledSource(String url, String distribution, String description) throws MalformedURLException {
+    final Source source = new Source();
+    source.setType(Source.Type.PACKAGE);
+    source.setDescription(description);
+    source.setEnabled(true);
+    source.setDistribution(distribution);
+    source.setUrl(new URL(url));
+    return source;
+  }
+
+  private Source createDisabledSource(String url, String distribution) throws MalformedURLException {
+    return createDisabledSource(url, distribution, null);
+  }
+
+  private Source createDisabledSource(String url, String distribution, String description) throws MalformedURLException {
+    final Source source = new Source();
+    source.setType(Source.Type.PACKAGE);
+    source.setDescription(description);
+    source.setEnabled(false);
+    source.setDistribution(distribution);
+    source.setUrl(new URL(url));
+    return source;
+  }
 }
