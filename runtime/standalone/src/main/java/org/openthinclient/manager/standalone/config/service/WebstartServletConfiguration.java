9000418f3b5c1140a61bb45eb4050594cd4c730f
Francois Fernandes <f.fernandes@fernandes-it.de>
2015-10-14 08:33:57 +0200

    Corrected URL mapping
    Wildcards may only appear at the beginning of the mapping, or the end. Not in between.

--- runtime/standalone/src/main/java/org/openthinclient/manager/standalone/config/service/WebstartServletConfiguration.java
+++ runtime/standalone/src/main/java/org/openthinclient/manager/standalone/config/service/WebstartServletConfiguration.java
@@ -4,22 +4,16 @@ import org.openthinclient.jnlp.servlet.JnlpDownloadServlet;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 
 @Configuration
 public class WebstartServletConfiguration {
 
   @Bean
-  @Order(10)
   public ServletRegistrationBean jnlpServlet() {
-
     final ServletRegistrationBean reg = new ServletRegistrationBean();
-
     reg.setServlet(new JnlpDownloadServlet());
-
-    reg.addUrlMappings("/console/*.jnlp");
-    reg.addUrlMappings("console/*.jar");
-
+    reg.setName("jnlpDownloadServlet");
+    reg.addUrlMappings("*.jnlp", "*.jar");
     return reg;
   }
 
