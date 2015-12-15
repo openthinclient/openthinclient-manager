cef542e31be6ff5e0f0d37b500d14df51bf3ff12
mogulsebbi <sebastian_kuehn@gmx.de>
2015-12-15 22:23:21 +0100

    added more complex methods for reseting package database and installation-directories. Until now the reseting of the packageManagers didn't finished correct.

--- services/package-manager/src/test/java/org/openthinclient/pkgmgr/it/PackageInstallTest.java
+++ services/package-manager/src/test/java/org/openthinclient/pkgmgr/it/PackageInstallTest.java
@@ -6,20 +6,32 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openthinclient.pkgmgr.DebianTestRepositoryServer;
 import org.openthinclient.pkgmgr.PackageManagerConfiguration;
+import org.openthinclient.pkgmgr.PackageManagerException;
 import org.openthinclient.pkgmgr.PackageManagerFactory;
 import org.openthinclient.pkgmgr.SimpleTargetDirectoryPackageManagerConfiguration;
 import org.openthinclient.util.dpkg.DPKGPackageManager;
+import org.openthinclient.util.dpkg.Package;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
@@ -32,6 +44,7 @@ import static org.junit.Assert.assertTrue;
 public class PackageInstallTest {
 
   private static DebianTestRepositoryServer testRepositoryServer;
+  
   @Autowired
   PackageManagerConfiguration configuration;
 
@@ -40,7 +53,7 @@ public class PackageInstallTest {
     testRepositoryServer = new DebianTestRepositoryServer();
     testRepositoryServer.start();
   }
-
+  
   @AfterClass
   public static void shutdownRepoServer() {
     testRepositoryServer.stop();
@@ -50,6 +63,7 @@ public class PackageInstallTest {
   @Test
   public void testInstallSinglePackage() throws Exception {
     String[] packages = {"foo", "zonk"};
+    
     for ( String pkgName : packages) {
       final DPKGPackageManager packageManager = preparePackageManager();
 
@@ -68,10 +82,12 @@ public class PackageInstallTest {
       for (Path file : pkgPath)
         assertFileExists(file);
       assertEquals("test-install-directory isn't empty",0,testInstallDirectory.toFile().listFiles().length);
+      
+      resetTestData("single", pkgName, packageManager);
   }
 }
  
-  /*@Test
+  @Test
   public void testInstallSinglePackageDependingOther() throws Exception {
     final DPKGPackageManager packageManager = preparePackageManager();
 
@@ -87,15 +103,17 @@ public class PackageInstallTest {
     final Path testInstallDirectory = configuration.getTestinstallDir().toPath();    
     
     Path[] fooPath = getFilePathsInPackage("foo", installDirectory);
-    Path[] barPath = getFilePathsInPackage("bar", installDirectory);
+    Path[] barPath = getFilePathsInPackage("bar2", installDirectory);
     for (Path file : fooPath)
     	assertFileExists(file);
     for (Path file: barPath)
     	assertFileExists(file);
     
     assertEquals(0,testInstallDirectory.toFile().listFiles().length);
+    
+    resetTestData("depending","foo-bar2", packageManager);
   }
-*/
+
   private DPKGPackageManager preparePackageManager() throws Exception {
     final DPKGPackageManager packageManager = PackageManagerFactory.createPackageManager(configuration);
   
@@ -107,11 +125,11 @@ public class PackageInstallTest {
     assertEquals("wrong URL of repository",testRepositoryServer.getServerUrl(), packageManager.getSourcesList().getSources().get(0).getUrl());
 
     //assertEquals(0, packageManager.getInstallablePackages().size());
-    clearDataBase(packageManager);
-    clearInstallDirectories();
     assertTrue("couldn't update cache-DB",packageManager.updateCacheDB());
     assertEquals("wrong number of installables packages",4, packageManager.getInstallablePackages().size());
 
+    saveDB();
+    
     return packageManager;
   }
 
@@ -121,23 +139,53 @@ private void writeSourcesList() throws Exception {
       out.write(("deb " + testRepositoryServer.getServerUrl().toExternalForm() + " ./").getBytes());
     }
   }
+
+  private void saveDB() throws Exception {
+	  File dbDir = configuration.getPackageDB().getParentFile();
+	  for (File dbFile : dbDir.listFiles()) {
+		  File outFile = new File(dbFile.getPath()+"-save");
+		  FileInputStream is = new FileInputStream(dbFile);
+		  FileOutputStream os = new FileOutputStream(outFile);
+		  while(is.available() != 0) {
+			  os.write(is.read());
+		  }
+		  is.close();
+		  os.close();
+	  }
+  }
   
-  private void clearDataBase(DPKGPackageManager packageManager) {
-	for (org.openthinclient.util.dpkg.Package pkg : packageManager.installedPackages.getPackages() ) {
-		packageManager.availablePackages.addPackage(pkg);
-		packageManager.installedPackages.removePackage(pkg);
-	}
+  private void restoreDB() throws Exception {
+	  File dbDir = configuration.getPackageDB().getParentFile();
+	  for (File dbFile : dbDir.listFiles()) {
+		  if (dbFile.getPath().contains("-save")) {
+			  File outFile = new File(dbFile.getPath().substring(0, dbFile.getPath().length() - "-save".length() ));
+			  FileInputStream is = new FileInputStream(dbFile);
+			  FileOutputStream os = new FileOutputStream(outFile);
+			  while(is.available() != 0) {
+				  os.write(is.read());
+			  }
+			  is.close();
+			  os.close();
+			  dbFile.delete();
+		  }
+	  }
   }
 
-  private void clearInstallDirectories() {
-	for(File currentFile : configuration.getInstallDir().listFiles()) {
-		currentFile.delete();
-	}
-	for(File currentFile : configuration.getTestinstallDir().listFiles()) {
-		currentFile.delete();
-	}
+  private void resetTestData(String testcase, String pkgNames, DPKGPackageManager manager) throws Exception {
+	File root = configuration.getInstallDir().getParentFile();
+	
+	File oldInstall = configuration.getInstallDir();
+	File newInstall = new File(root, "install");
+	File oldTestInstall = configuration.getTestinstallDir();
+	File newTestInstall = new File(root, "install-test");
+	oldInstall.renameTo(new File(oldInstall.toString() + "-" + testcase + "-" + pkgNames));
+	oldTestInstall.renameTo(new File(oldTestInstall.toString() + "-" + testcase + "-" + pkgNames));	
+	newInstall.mkdir();
+	newTestInstall.mkdir();
+	
+	restoreDB();
   }
-
+  
   @Configuration()
   @Import(SimpleTargetDirectoryPackageManagerConfiguration.class)
   public static class PackageManagerConfig {
