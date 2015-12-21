223d2024dd15a9c9d5efc809756a05f0273b387c
Joerg Neumann <Joerg.Neumann@t-systems.com>
2015-12-21 23:02:43 +0100

    OTC-Dashboard working state

---
+++ console-web/webapp/src/main/java/org/openthinclient/web/Application.java
@@ -0,0 +1,141 @@
+package org.openthinclient.web;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.vaadin.spring.http.HttpService;
+import org.vaadin.spring.i18n.MessageProvider;
+import org.vaadin.spring.i18n.ResourceBundleMessageProvider;
+import org.vaadin.spring.security.annotation.EnableVaadinSharedSecurity;
+import org.vaadin.spring.security.config.VaadinSharedSecurityConfiguration;
+import org.vaadin.spring.security.web.VaadinRedirectStrategy;
+import org.vaadin.spring.security.web.authentication.VaadinAuthenticationSuccessHandler;
+import org.vaadin.spring.security.web.authentication.VaadinUrlAuthenticationSuccessHandler;
+
+import com.vaadin.server.CustomizedSystemMessages;
+import com.vaadin.server.SystemMessages;
+import com.vaadin.server.SystemMessagesInfo;
+import com.vaadin.server.SystemMessagesProvider;
+
+/**
+ *
+ */
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class)
+public class Application {
+ 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * Provide custom system messages to make sure the application is reloaded when the session expires.
+     *
+     * @return SystemMessagesProvider
+     */
+    @Bean
+    SystemMessagesProvider systemMessagesProvider() {
+        return new SystemMessagesProvider() {
+            /** serialVersionUID */
+            private static final long serialVersionUID = 2570216527087874367L;
+
+            @Override
+            public SystemMessages getSystemMessages(SystemMessagesInfo systemMessagesInfo) {
+                CustomizedSystemMessages systemMessages = new CustomizedSystemMessages();
+                systemMessages.setSessionExpiredNotificationEnabled(false);
+                return systemMessages;
+            }
+        };
+    }
+
+    @Bean
+    MessageProvider communicationMessages() {
+        return new ResourceBundleMessageProvider("i18n/messages"); // Will use UTF-8 by default
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.GERMAN);
+        return slr;
+    }
+
+    /**
+     * Configure Spring Security.
+     */
+    @Configuration
+    @EnableWebSecurity
+    @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+    @EnableVaadinSharedSecurity
+    static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        public void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication()
+                    .withUser("user").password("user").roles("USER")
+                    .and()
+                    .withUser("admin").password("admin").roles("ADMIN");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable(); // Use Vaadin's built-in CSRF protection instead
+            http.authorizeRequests()
+                    .antMatchers("/login/**").anonymous()
+                    .antMatchers("/vaadinServlet/UIDL/**").permitAll()
+                    .antMatchers("/vaadinServlet/HEARTBEAT/**").permitAll()
+                    .anyRequest().authenticated();
+            http.httpBasic().disable();
+            http.formLogin().disable();
+            http.logout()
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/login?logout")
+                    .permitAll();
+            http.exceptionHandling()
+                    .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"));
+            http.rememberMe().rememberMeServices(rememberMeServices()).key("myAppKey");
+        }
+
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring().antMatchers("/VAADIN/**");
+        }
+
+        @Override
+        @Bean
+        public AuthenticationManager authenticationManagerBean() throws Exception {
+            return super.authenticationManagerBean();
+        }
+
+        @Bean
+        public RememberMeServices rememberMeServices() {
+            // TODO Is there some way of exposing the RememberMeServices instance that the remember me configurer creates by default?
+            TokenBasedRememberMeServices services = new TokenBasedRememberMeServices("myAppKey", userDetailsService());
+            services.setAlwaysRemember(true);
+            return services;
+        }
+
+        @Bean(name = VaadinSharedSecurityConfiguration.VAADIN_AUTHENTICATION_SUCCESS_HANDLER_BEAN)
+        VaadinAuthenticationSuccessHandler vaadinAuthenticationSuccessHandler(HttpService httpService, VaadinRedirectStrategy vaadinRedirectStrategy) {
+            return new VaadinUrlAuthenticationSuccessHandler(httpService, vaadinRedirectStrategy, "/");
+        }
+    }
+}
---
+++ console-web/webapp/src/main/java/org/openthinclient/web/event/DashboardEvent.java
@@ -0,0 +1,85 @@
+package org.openthinclient.web.event;
+
+import java.util.Collection;
+
+import org.openthinclient.web.domain.Transaction;
+import org.openthinclient.web.view.DashboardViewType;
+
+/*
+ * Event bus events used in Dashboard are listed here as inner classes.
+ */
+public abstract class DashboardEvent {
+
+    public static final class UserLoginRequestedEvent {
+        private final String userName, password;
+
+        public UserLoginRequestedEvent(final String userName,
+                final String password) {
+            this.userName = userName;
+            this.password = password;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
+
+    public static class BrowserResizeEvent {
+
+    }
+
+    public static class UserLoggedOutEvent {
+
+    }
+
+    public static class NotificationsCountUpdatedEvent {
+    }
+
+    public static final class ReportsCountUpdatedEvent {
+        private final int count;
+
+        public ReportsCountUpdatedEvent(final int count) {
+            this.count = count;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+    }
+
+    public static final class TransactionReportEvent {
+        private final Collection<Transaction> transactions;
+
+        public TransactionReportEvent(final Collection<Transaction> transactions) {
+            this.transactions = transactions;
+        }
+
+        public Collection<Transaction> getTransactions() {
+            return transactions;
+        }
+    }
+
+    public static final class PostViewChangeEvent {
+        private final DashboardViewType view;
+
+        public PostViewChangeEvent(final DashboardViewType view) {
+            this.view = view;
+        }
+
+        public DashboardViewType getView() {
+            return view;
+        }
+    }
+
+    public static class CloseOpenWindowsEvent {
+    }
+
+    public static class ProfileUpdatedEvent {
+    }
+
+}
