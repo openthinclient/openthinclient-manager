c6de92f17b03e855a479eab74d2f76b2fb45de81
Joerg Neumann <neumann.jorg@googlemail.com>
2016-01-07 22:07:13 +0100

    Eclipse-formatter

--- console-web/webapp/src/main/java/org/openthinclient/web/WebApplicationConfiguration.java
+++ console-web/webapp/src/main/java/org/openthinclient/web/WebApplicationConfiguration.java
@@ -2,33 +2,16 @@ package org.openthinclient.web;
 
 import java.util.Locale;
 
-import org.openthinclient.service.apacheds.DirectoryServiceConfiguration;
-import org.openthinclient.service.common.home.impl.ManagerHomeFactory;
 import org.openthinclient.web.view.DashboardSections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.authentication.RememberMeServices;
-import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.vaadin.spring.http.HttpService;
 import org.vaadin.spring.i18n.MessageProvider;
 import org.vaadin.spring.i18n.ResourceBundleMessageProvider;
-import org.vaadin.spring.security.annotation.EnableVaadinSharedSecurity;
-import org.vaadin.spring.security.config.VaadinSharedSecurityConfiguration;
-import org.vaadin.spring.security.web.VaadinRedirectStrategy;
-import org.vaadin.spring.security.web.authentication.VaadinAuthenticationSuccessHandler;
-import org.vaadin.spring.security.web.authentication.VaadinUrlAuthenticationSuccessHandler;
 import org.vaadin.spring.sidebar.annotation.EnableSideBar;
 
 import com.vaadin.server.CustomizedSystemMessages;
@@ -41,125 +24,50 @@ import com.vaadin.server.SystemMessagesProvider;
  */
 @SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class)
 @EnableSideBar
+@Import({WebApplicationSecurityConfiguration.class})
 public class WebApplicationConfiguration {
- 
-    private static final Logger LOGGER = LoggerFactory.getLogger(WebApplicationConfiguration.class);
 
-    /**
-     * Provide custom system messages to make sure the application is reloaded when the session expires.
-     *
-     * @return SystemMessagesProvider
-     */
-    @Bean
-    SystemMessagesProvider systemMessagesProvider() {
-        return new SystemMessagesProvider() {
-            /** serialVersionUID */
-            private static final long serialVersionUID = 2570216527087874367L;
+   /**
+    * Provide custom system messages to make sure the application is reloaded
+    * when the session expires.
+    *
+    * @return SystemMessagesProvider
+    */
+   @Bean
+   SystemMessagesProvider systemMessagesProvider() {
+      return new SystemMessagesProvider() {
 
-            @Override
-            public SystemMessages getSystemMessages(SystemMessagesInfo systemMessagesInfo) {
-                CustomizedSystemMessages systemMessages = new CustomizedSystemMessages();
-                systemMessages.setSessionExpiredNotificationEnabled(false);
-                return systemMessages;
-            }
-        };
-    }
+         /** serialVersionUID */
+         private static final long serialVersionUID = 2570216527087874367L;
 
-    @Bean
-    MessageProvider communicationMessages() {
-        return new ResourceBundleMessageProvider("i18n/messages"); // Will use UTF-8 by default
-    }
+         @Override
+         public SystemMessages getSystemMessages(SystemMessagesInfo systemMessagesInfo) {
+            CustomizedSystemMessages systemMessages = new CustomizedSystemMessages();
+            systemMessages.setSessionExpiredNotificationEnabled(false);
+            return systemMessages;
+         }
+      };
+   }
 
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.GERMAN);
-        return slr;
-    }
+   @Bean
+   MessageProvider communicationMessages() {
+      return new ResourceBundleMessageProvider("i18n/messages"); // Will use UTF-8 by default
+   }
 
-    /**
-     * Creates the DashboardSections meta bean. This bean is only required for the {@link org.vaadin.spring.sidebar.SideBarUtils}
-     * to pickup the defined dashboard sections.
-     */
-    @Bean
-    public DashboardSections dashboardSections() {
-        return new DashboardSections();
-    }
+   @Bean
+   public LocaleResolver localeResolver() {
+      SessionLocaleResolver slr = new SessionLocaleResolver();
+      slr.setDefaultLocale(Locale.GERMAN);
+      return slr;
+   }
 
-    /**
-     * Configure Spring Security.
-     */
-    @EnableWebSecurity
-    @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, proxyTargetClass = true)
-    @EnableVaadinSharedSecurity
-    static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-      @Bean
-      public DirectoryServiceConfiguration directoryServiceConfiguration() {
-        // only for testing. runtime standalone has a more sophisticated setup
-        return new ManagerHomeFactory().create().getConfiguration(DirectoryServiceConfiguration.class);
-      }
-    	
-        @Override
-        public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//            auth.inMemoryAuthentication()
-//                    .withUser("user").password("user").roles("USER")
-//                    .and()
-//                    .withUser("admin").password("admin").roles("ADMIN");
-        	
-        	DirectoryServiceConfiguration dsc = directoryServiceConfiguration();
-        	String ldapUrl = "ldap://localhost:" + dsc.getEmbeddedLdapPort() + "/" + dsc.getEmbeddedCustomRootPartitionName();
-        	
-        auth.ldapAuthentication()
-        .userDnPatterns("cn={0}")
-        .userSearchBase("ou=users")
-//        .groupSearchBase("ou=groups")
-        .contextSource()
-//            .ldif("classpath:test-server.ldif")
-            .url(ldapUrl);
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable(); // Use Vaadin's built-in CSRF protection instead
-            http.authorizeRequests()
-                    .antMatchers("/login/**").anonymous()
-                    .antMatchers("/vaadinServlet/UIDL/**").permitAll()
-                    .antMatchers("/vaadinServlet/HEARTBEAT/**").permitAll()
-                    .anyRequest().authenticated();
-            http.httpBasic().disable();
-            http.formLogin().disable();
-            http.logout()
-                    .logoutUrl("/logout")
-                    .logoutSuccessUrl("/login?logout")
-                    .permitAll();
-            http.exceptionHandling()
-                    .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"));
-            http.rememberMe().rememberMeServices(rememberMeServices()).key("myAppKey");
-        }
-
-        @Override
-        public void configure(WebSecurity web) throws Exception {
-            web.ignoring().antMatchers("/VAADIN/**");
-        }
-
-        @Override
-        @Bean
-        public AuthenticationManager authenticationManagerBean() throws Exception {
-            return super.authenticationManagerBean();
-        }
-
-        @Bean
-        public RememberMeServices rememberMeServices() {
-            // TODO Is there some way of exposing the RememberMeServices instance that the remember me configurer creates by default?
-            TokenBasedRememberMeServices services = new TokenBasedRememberMeServices("myAppKey", userDetailsService());
-            services.setAlwaysRemember(true);
-            return services;
-        }
-
-        @Bean(name = VaadinSharedSecurityConfiguration.VAADIN_AUTHENTICATION_SUCCESS_HANDLER_BEAN)
-        VaadinAuthenticationSuccessHandler vaadinAuthenticationSuccessHandler(HttpService httpService, VaadinRedirectStrategy vaadinRedirectStrategy) {
-            return new VaadinUrlAuthenticationSuccessHandler(httpService, vaadinRedirectStrategy, "/");
-        }
-    }    
+   /**
+    * Creates the DashboardSections meta bean. This bean is only required for
+    * the {@link org.vaadin.spring.sidebar.SideBarUtils} to pickup the defined
+    * dashboard sections.
+    */
+   @Bean
+   public DashboardSections dashboardSections() {
+      return new DashboardSections();
+   }
 }
