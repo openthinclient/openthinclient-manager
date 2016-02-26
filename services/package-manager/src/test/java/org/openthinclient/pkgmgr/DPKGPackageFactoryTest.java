a6155963114cd1f28b5d2c6477edb1e9871f26b9
sebkuehn <sebastian_kuehn@gmx.de>
2016-02-26 08:40:31 +0100

    changed PackageFactory-Tests to match the new created set of packages

--- services/package-manager/src/test/java/org/openthinclient/pkgmgr/DPKGPackageFactoryTest.java
+++ services/package-manager/src/test/java/org/openthinclient/pkgmgr/DPKGPackageFactoryTest.java
@@ -13,20 +13,61 @@ public class DPKGPackageFactoryTest {
   public void testPackagesParsing() throws Exception {
     final List<org.openthinclient.util.dpkg.Package> packageList = new DPKGPackageFactory(null).getPackage(getClass().getResourceAsStream("/test-repository/Packages"));
 
-    assertEquals(5, packageList.size());
+    assertEquals(16, packageList.size());
 
     assertEquals("foo", packageList.get(0).getName());
-    
+    assertEquals(new ANDReference("foo2"), packageList.get(0).getConflicts());
+    assertEquals(new ANDReference("foo"), packageList.get(0).getProvides());
+
     assertEquals("foo", packageList.get(1).getName());
+    assertEquals(new ANDReference("foo2"), packageList.get(1).getConflicts());
     
-    assertEquals("zonk", packageList.get(2).getName());
-    assertEquals(new ANDReference("bar2"), packageList.get(2).getConflicts());
+    assertEquals("foo-fork", packageList.get(2).getName());
+    assertEquals(new ANDReference("zonk"), packageList.get(2).getConflicts());
+    assertEquals(new ANDReference("foo"), packageList.get(2).getProvides());
 
-    assertEquals("bar2", packageList.get(3).getName());
-    assertEquals(new ANDReference("foo"), packageList.get(3).getDepends());
+    assertEquals("foo2", packageList.get(3).getName());
+    assertEquals(new ANDReference("foo"), packageList.get(3).getConflicts());
+
+    assertEquals("zonk", packageList.get(4).getName());
+    assertEquals(new ANDReference("bar2"), packageList.get(4).getConflicts());
+
+    assertEquals("zonk", packageList.get(5).getName());
+    assertEquals(new ANDReference("foo"), packageList.get(5).getDepends());
+    assertEquals(new ANDReference("bar2"), packageList.get(5).getConflicts());
     
-    assertEquals("bar", packageList.get(4).getName());
-    assertEquals(new ANDReference("foo (>= 0:2.1-1)"), packageList.get(4).getDepends());
+    assertEquals("zonk-dev", packageList.get(6).getName());
+    assertEquals(new ANDReference("foo"), packageList.get(6).getConflicts());
 
+    assertEquals("zonk2", packageList.get(7).getName());
+    assertEquals(new ANDReference("bas2"), packageList.get(7).getDepends());
+
+    assertEquals("bar", packageList.get(8).getName());
+    assertEquals(new ANDReference("foo (>= 2.1-1)"), packageList.get(8).getDepends());
+    assertEquals(new ANDReference("zonk (<= 2.0-1)"), packageList.get(8).getConflicts());
+    
+    assertEquals("bar2", packageList.get(9).getName());
+    assertEquals(new ANDReference("foo"), packageList.get(9).getDepends());
+    
+    assertEquals("bar2", packageList.get(10).getName());
+    assertEquals(new ANDReference("foo"), packageList.get(10).getConflicts());
+    
+    assertEquals("bar2-dev", packageList.get(11).getName());
+    assertEquals(new ANDReference("bar2"), packageList.get(11).getDepends());
+    assertEquals(new ANDReference("foo (<= 2.0-1)"), packageList.get(11).getConflicts());
+    
+    assertEquals("bas", packageList.get(12).getName());
+    assertEquals(new ANDReference("foo (>= 2.0-1)"), packageList.get(12).getDepends());
+    
+    assertEquals("bas", packageList.get(13).getName());
+    assertEquals(new ANDReference("foo (>= 2.1-1)"), packageList.get(13).getDepends());
+    
+    assertEquals("bas-dev", packageList.get(14).getName());
+    assertEquals(new ANDReference("foo"), packageList.get(14).getDepends());
+    assertEquals(new ANDReference("foo"), packageList.get(14).getConflicts());
+    
+    assertEquals("bas2", packageList.get(15).getName());
+    assertEquals(new ANDReference("zonk2"), packageList.get(15).getDepends());
+    
   }
 }
--- services/package-manager/src/test/java/org/openthinclient/util/dpkg/DPKGPackageFactoryTest.java
+++ services/package-manager/src/test/java/org/openthinclient/util/dpkg/DPKGPackageFactoryTest.java
@@ -18,14 +18,40 @@ public class DPKGPackageFactoryTest {
     final DPKGPackageFactory factory = new DPKGPackageFactory(null);
     final List<Package> packages = factory.getPackage(packagesStream);
 
-    assertEquals(5, packages.size());
+    assertEquals(16, packages.size());
 
     assertEquals("foo", packages.get(0).getName());
+    assertEquals(new Version("2.0-1"), packages.get(0).getVersion());
     assertEquals("foo", packages.get(1).getName());
-    assertEquals("zonk", packages.get(2).getName());
-    assertEquals("bar2", packages.get(3).getName());
-    assertEquals("bar", packages.get(4).getName());
-
+    assertEquals(new Version("2.1-1"), packages.get(1).getVersion());
+    assertEquals("foo-fork", packages.get(2).getName());
+    assertEquals(new Version("2.0-1"), packages.get(2).getVersion());
+    assertEquals("foo2", packages.get(3).getName());
+    assertEquals(new Version("2.0-1"), packages.get(3).getVersion());
+    assertEquals("zonk", packages.get(4).getName());
+    assertEquals(new Version("2.0-1"), packages.get(4).getVersion());
+    assertEquals("zonk", packages.get(5).getName());
+    assertEquals(new Version("2.1-1"), packages.get(5).getVersion());
+    assertEquals("zonk-dev", packages.get(6).getName());
+    assertEquals(new Version("2.0-1"), packages.get(6).getVersion());
+    assertEquals("zonk2", packages.get(7).getName());
+    assertEquals(new Version("2.0-1"), packages.get(7).getVersion());
+    assertEquals("bar", packages.get(8).getName());
+    assertEquals(new Version("2.0-1"), packages.get(8).getVersion());
+    assertEquals("bar2", packages.get(9).getName());
+    assertEquals(new Version("2.0-1"), packages.get(9).getVersion());
+    assertEquals("bar2", packages.get(10).getName());
+    assertEquals(new Version("2.1-1"), packages.get(10).getVersion());
+    assertEquals("bar2-dev", packages.get(11).getName());
+    assertEquals(new Version("2.0-1"), packages.get(11).getVersion());
+    assertEquals("bas", packages.get(12).getName());
+    assertEquals(new Version("2.0-1"), packages.get(12).getVersion());
+    assertEquals("bas", packages.get(13).getName());
+    assertEquals(new Version("2.1-1"), packages.get(13).getVersion());
+    assertEquals("bas-dev", packages.get(14).getName());
+    assertEquals(new Version("2.0-1"), packages.get(14).getVersion());
+    assertEquals("bas2", packages.get(15).getName());
+    assertEquals(new Version("2.0-1"), packages.get(15).getVersion());
 
   }
 }
