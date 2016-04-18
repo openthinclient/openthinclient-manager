af9350f45c430cf80b3d4399adc336779aa15909
Francois Fernandes <f.fernandes@fernandes-it.de>
2016-04-18 07:54:23 +0200

    Update Sources implemented and rough state management

--- console-web/webapp/src/main/java/org/openthinclient/web/pkgmngr/ui/SourcesListNavigatorView.java
+++ console-web/webapp/src/main/java/org/openthinclient/web/pkgmngr/ui/SourcesListNavigatorView.java
@@ -5,37 +5,53 @@ import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
+
 import org.openthinclient.pkgmgr.PackageManager;
 import org.openthinclient.web.pkgmngr.ui.presenter.SourcesListPresenter;
 import org.openthinclient.web.pkgmngr.ui.view.SourcesListView;
 import org.openthinclient.web.view.DashboardSections;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.spring.events.EventBus;
 import org.vaadin.spring.sidebar.annotation.SideBarItem;
 
-@SpringView(name="sources")
+@SpringView(name = "sources")
 @SideBarItem(sectionId = DashboardSections.PACKAGE_MANAGEMENT, caption = "Package Sources")
 public class SourcesListNavigatorView extends Panel implements View {
 
-  private final SourcesListPresenter presenter;
+    private final SourcesListPresenter presenter;
 
-  @Autowired
-  PackageManager packageManager;
+    @Autowired
+    PackageManager packageManager;
+    @Autowired
+    EventBus.SessionEventBus eventBus;
 
-  public SourcesListNavigatorView() {
-    final VerticalLayout content = new VerticalLayout();
-    final SourcesListView sourcesListView = new SourcesListView();
+    public SourcesListNavigatorView() {
+        final VerticalLayout content = new VerticalLayout();
+        final SourcesListView sourcesListView = new SourcesListView();
 
-    presenter = new SourcesListPresenter(sourcesListView);
+        presenter = new SourcesListPresenter(sourcesListView);
 
-    content.addComponent(sourcesListView);
-    content.setSizeFull();
-    setContent(content);
-  }
+        content.addComponent(sourcesListView);
+        content.setSizeFull();
+        setContent(content);
+    }
 
-  @Override
-  public void enter(ViewChangeListener.ViewChangeEvent event) {
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent event) {
 
-    presenter.setPackageManager(packageManager);
+        presenter.setPackageManager(packageManager);
 
-  }
+    }
+
+    @Override
+    public void attach() {
+        super.attach();
+        eventBus.subscribe(presenter);
+    }
+
+    @Override
+    public void detach() {
+        eventBus.unsubscribe(presenter);
+        super.detach();
+    }
 }
--- console-web/webapp/src/main/java/org/openthinclient/web/pkgmngr/ui/presenter/SourcesListPresenter.java
+++ console-web/webapp/src/main/java/org/openthinclient/web/pkgmngr/ui/presenter/SourcesListPresenter.java
@@ -5,168 +5,193 @@ import com.vaadin.data.Property;
 import com.vaadin.data.fieldgroup.FieldGroup;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.Page;
-import com.vaadin.ui.*;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CheckBox;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.TextArea;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.themes.ValoTheme;
+
 import org.openthinclient.pkgmgr.PackageManager;
 import org.openthinclient.pkgmgr.db.Source;
 import org.openthinclient.pkgmgr.db.SourceRepository;
+import org.openthinclient.web.ui.event.PackageManagerTaskActivatedEvent;
+import org.openthinclient.web.ui.event.PackageManagerTaskFinalizedEvent;
+import org.vaadin.spring.events.Event;
+import org.vaadin.spring.events.annotation.EventBusListenerMethod;
 
 public class SourcesListPresenter {
 
-  private final View view;
-  private final FieldGroup sourceFormBinder;
-  private final BeanItemContainer<Source> container;
-  private PackageManager packageManager;
+    private final View view;
+    private final FieldGroup sourceFormBinder;
+    private final BeanItemContainer<Source> container;
+    private PackageManager packageManager;
 
-  public SourcesListPresenter(View view) {
-    this.view = view;
+    public SourcesListPresenter(View view) {
+        this.view = view;
 
-    sourceFormBinder = new FieldGroup();
-    sourceFormBinder.bind(view.getURLTextField(), "url");
-    sourceFormBinder.bind(view.getDescriptionTextArea(), "description");
-    sourceFormBinder.bind(view.getEnabledCheckBox(), "enabled");
+        sourceFormBinder = new FieldGroup();
+        sourceFormBinder.bind(view.getURLTextField(), "url");
+        sourceFormBinder.bind(view.getDescriptionTextArea(), "description");
+        sourceFormBinder.bind(view.getEnabledCheckBox(), "enabled");
 
-    view.getURLTextField().setConverter(new StringToUrlConverter());
+        view.getURLTextField().setConverter(new StringToUrlConverter());
 
-    container = new BeanItemContainer<>(Source.class);
-    this.view.getSourcesTable().setContainerDataSource(container);
-    // FIXME that should be part of the view, not the presenter
-    view.getSourcesTable().setVisibleColumns("url");
+        container = new BeanItemContainer<>(Source.class);
+        this.view.getSourcesTable().setContainerDataSource(container);
+        // FIXME that should be part of the view, not the presenter
+        view.getSourcesTable().setVisibleColumns("url");
 
-    this.view.getSourcesTable().addValueChangeListener(this::sourcesListValueChanged);
-    this.view.getSaveSourceButton().addClickListener(this::saveSourcesClicked);
-    this.view.getAddSourceButton().addClickListener(this::addSourceClicked);
-    this.view.getDeleteSourceButton().addClickListener(this::removeSourceClicked);
-  }
-
-  private void removeSourceClicked(Button.ClickEvent clickEvent) {
-
-    // FIXME add some kind of confirm dialog
-    Source source = view.getSelectedSource();
-    sourceSelected(null);
-    container.removeItem(source);
-
-    if (source.getId() == null) {
-      // this source has not yet been added to the repository.
-      return;
-    } else {
-      // the source has already been persisted. Remove the entity from the database
-      packageManager.getSourceRepository().delete(source);
+        this.view.getSourcesTable().addValueChangeListener(this::sourcesListValueChanged);
+        this.view.getSaveSourceButton().addClickListener(this::saveSourcesClicked);
+        this.view.getAddSourceButton().addClickListener(this::addSourceClicked);
+        this.view.getDeleteSourceButton().addClickListener(this::removeSourceClicked);
+        this.view.getUpdateButton().addClickListener(this::updateSourcesClicked);
     }
 
-    view.refreshSourcesList();
-
-  }
-
-  private void addSourceClicked(Button.ClickEvent clickEvent) {
-
-    final Source newSource = new Source();
-    newSource.setEnabled(true);
-    newSource.setDescription("Newly created source");
-    container.addItem(newSource);
-    sourceSelected(newSource);
-
-  }
-
-  private void saveSourcesClicked(Button.ClickEvent clickEvent) {
-
-    // validate the current source
-    try {
-      sourceFormBinder.commit();
-    } catch (FieldGroup.CommitException e) {
-      e.printStackTrace();
-      return;
+    private void updateSourcesClicked(Button.ClickEvent clickEvent) {
+        packageManager.updateCacheDB();
     }
 
-    Source source = view.getSelectedSource();
-    final int idx = container.indexOfId(source);
-    source = packageManager.getSourceRepository().saveAndFlush(source);
+    private void removeSourceClicked(Button.ClickEvent clickEvent) {
 
-    // FIXME move that to something centrally and more managed!
-    final Notification notification = new Notification("Package Sources Saved",
-          "Your configuration has been successfully saved. Please do not forget to run update to reload the package cache.", Notification.Type.HUMANIZED_MESSAGE);
-    notification.setStyleName(ValoTheme.NOTIFICATION_BAR + " " + ValoTheme.NOTIFICATION_SUCCESS);
-    notification.show(Page.getCurrent());
+        // FIXME add some kind of confirm dialog
+        Source source = view.getSelectedSource();
+        sourceSelected(null);
+        container.removeItem(source);
 
+        if (source.getId() == null) {
+            // this source has not yet been added to the repository.
+            return;
+        } else {
+            // the source has already been persisted. Remove the entity from the database
+            packageManager.getSourceRepository().delete(source);
+        }
 
-    container.removeItem(idx);
-    container.addItemAt(idx, source);
-
-    this.view.refreshSourcesList();
-  }
-
-
-  private void sourcesListValueChanged(Property.ValueChangeEvent valueChangeEvent) {
-
-    final Source selectedSource = (Source) view.getSourcesTable().getValue();
-
-    sourceSelected(selectedSource);
-  }
-
-
-  private void sourceSelected(Source source) {
-
-    view.getSourcesTable().setValue(source);
-
-    if (source == null) {
-      // reset
-
-      sourceFormBinder.setEnabled(false);
-      sourceFormBinder.setItemDataSource(null);
-
-    } else {
-
-      sourceFormBinder.setEnabled(true);
-
-      final Item sourceItem = getSourceItem(source);
-      sourceFormBinder.setItemDataSource(sourceItem);
+        view.refreshSourcesList();
 
     }
 
-  }
+    private void addSourceClicked(Button.ClickEvent clickEvent) {
 
-  protected Item getSourceItem(Source source) {
-    return container.getItem(source);
-  }
+        final Source newSource = new Source();
+        newSource.setEnabled(true);
+        newSource.setDescription("Newly created source");
+        container.addItem(newSource);
+        sourceSelected(newSource);
 
-  public void setPackageManager(PackageManager packageManager) {
-    this.packageManager = packageManager;
-
-    updateSources(packageManager != null ? packageManager.getSourceRepository() : null);
-
-  }
-
-  private void updateSources(SourceRepository repository) {
-
-    container.removeAllItems();
-    if (repository != null) {
-      container.addAll(repository.findAll());
     }
 
-    sourceSelected(null);
+    private void saveSourcesClicked(Button.ClickEvent clickEvent) {
 
-  }
+        // validate the current source
+        try {
+            sourceFormBinder.commit();
+        } catch (FieldGroup.CommitException e) {
+            e.printStackTrace();
+            return;
+        }
 
-  public interface View {
+        Source source = view.getSelectedSource();
+        final int idx = container.indexOfId(source);
+        source = packageManager.getSourceRepository().saveAndFlush(source);
 
-    Button getUpdateButton();
+        // FIXME move that to something centrally and more managed!
+        final Notification notification = new Notification("Package Sources Saved",
+                "Your configuration has been successfully saved. Please do not forget to run update to reload the package cache.", Notification.Type.HUMANIZED_MESSAGE);
+        notification.setStyleName(ValoTheme.NOTIFICATION_BAR + " " + ValoTheme.NOTIFICATION_SUCCESS);
+        notification.show(Page.getCurrent());
 
-    Button getAddSourceButton();
 
-    Button getDeleteSourceButton();
+        container.removeItem(idx);
+        container.addItemAt(idx, source);
 
-    Button getSaveSourceButton();
+        this.view.refreshSourcesList();
+    }
 
-    TextField getURLTextField();
 
-    CheckBox getEnabledCheckBox();
+    private void sourcesListValueChanged(Property.ValueChangeEvent valueChangeEvent) {
 
-    TextArea getDescriptionTextArea();
+        final Source selectedSource = (Source) view.getSourcesTable().getValue();
 
-    Table getSourcesTable();
+        sourceSelected(selectedSource);
+    }
 
-    void refreshSourcesList();
 
-    Source getSelectedSource();
-  }
+    private void sourceSelected(Source source) {
+
+        view.getSourcesTable().setValue(source);
+
+        if (source == null) {
+            // reset
+
+            sourceFormBinder.setEnabled(false);
+            sourceFormBinder.setItemDataSource(null);
+
+        } else {
+
+            sourceFormBinder.setEnabled(true);
+
+            final Item sourceItem = getSourceItem(source);
+            sourceFormBinder.setItemDataSource(sourceItem);
+
+        }
+
+    }
+
+    protected Item getSourceItem(Source source) {
+        return container.getItem(source);
+    }
+
+    public void setPackageManager(PackageManager packageManager) {
+        this.packageManager = packageManager;
+
+        updateSources(packageManager != null ? packageManager.getSourceRepository() : null);
+
+    }
+
+    private void updateSources(SourceRepository repository) {
+
+        container.removeAllItems();
+        if (repository != null) {
+            container.addAll(repository.findAll());
+        }
+
+        sourceSelected(null);
+
+    }
+
+    @EventBusListenerMethod
+    public void onPackageManagerTaskActivated(Event<PackageManagerTaskActivatedEvent> event) {
+        view.getUpdateButton().setEnabled(false);
+    }
+
+    @EventBusListenerMethod
+    public void onPackageManagerTaskFinalized(Event<PackageManagerTaskFinalizedEvent> event) {
+        view.getUpdateButton().setEnabled(true);
+    }
+
+    public interface View {
+
+        Button getUpdateButton();
+
+        Button getAddSourceButton();
+
+        Button getDeleteSourceButton();
+
+        Button getSaveSourceButton();
+
+        TextField getURLTextField();
+
+        CheckBox getEnabledCheckBox();
+
+        TextArea getDescriptionTextArea();
+
+        Table getSourcesTable();
+
+        void refreshSourcesList();
+
+        Source getSelectedSource();
+    }
 }
