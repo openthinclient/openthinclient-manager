44a6a83aa36504c3197591765b1e06708bae198b
Francois Fernandes <f.fernandes@fernandes-it.de>
2016-04-27 23:53:07 +0200

    Verify that only a single source will be set

--- services/package-manager/src/test/java/org/openthinclient/pkgmgr/PackageTestUtils.java
+++ services/package-manager/src/test/java/org/openthinclient/pkgmgr/PackageTestUtils.java
@@ -1,8 +1,12 @@
 package org.openthinclient.pkgmgr;
 
+import org.junit.Assert;
 import org.openthinclient.pkgmgr.db.Package;
 import org.openthinclient.pkgmgr.db.Source;
+import org.openthinclient.pkgmgr.db.SourceRepository;
 import org.openthinclient.pkgmgr.db.Version;
+
+import java.util.List;
 
 public class PackageTestUtils {
     public static Package createPackage(String name, String version) {
@@ -20,11 +24,23 @@ public class PackageTestUtils {
     }
 
     public static void configureSources(DebianTestRepositoryServer server, PackageManager packageManager) {
+        final SourceRepository sourceRepository = packageManager.getSourceRepository();
 
-        final Source source = new Source();
-        source.setEnabled(true);
-        source.setUrl(server.getServerUrl());
-        packageManager.getSourceRepository().saveAndFlush(source);
+        final List<Source> existing = sourceRepository.findAll();
+
+        if (existing.size() == 0) {
+            final Source source = new Source();
+            source.setEnabled(true);
+            source.setUrl(server.getServerUrl());
+            sourceRepository.saveAndFlush(source);
+
+        } else if (existing.size() == 1) {
+            final Source existingSource = existing.get(0);
+            Assert.assertEquals(server.getServerUrl(), existingSource.getUrl());
+        } else {
+            Assert.fail("More than a single source has been registered");
+        }
+
 
     }
 }
