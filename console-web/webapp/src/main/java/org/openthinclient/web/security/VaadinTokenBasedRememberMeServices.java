0dcd48a384065e3167074ba7558f2c8c45d199c2
Joerg Neumann <neumann.jorg@googlemail.com>
2016-05-01 08:18:43 +0200

    Remember-me enabled

---
+++ console-web/webapp/src/main/java/org/openthinclient/web/security/VaadinTokenBasedRememberMeServices.java
@@ -0,0 +1,32 @@
+package org.openthinclient.web.security;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+
+/**
+ * The TokenBasedRememberMeServices use httpRequest.getParameter to check where remember-me is requested or not. 
+ * We can only add request-attributes to httpRequest, so we need to overwrite method rememberMeRequested().
+ * 
+ * @author jne
+ *
+ */
+public class VaadinTokenBasedRememberMeServices extends TokenBasedRememberMeServices {
+
+  public VaadinTokenBasedRememberMeServices(String key, UserDetailsService userDetailsService) {
+    super(key, userDetailsService);
+  }
+
+  @Override
+  protected boolean rememberMeRequested(HttpServletRequest request, String parameter) {
+    
+    if (request.getAttribute(DEFAULT_PARAMETER) != null
+        && (boolean) request.getAttribute(DEFAULT_PARAMETER)) {
+      
+      request.removeAttribute(DEFAULT_PARAMETER);
+      return true;
+    }
+    return super.rememberMeRequested(request, parameter);
+  }
+}
--- console-web/webapp/src/main/java/org/openthinclient/web/ui/LoginView.java
+++ console-web/webapp/src/main/java/org/openthinclient/web/ui/LoginView.java
@@ -1,16 +1,31 @@
 
 package org.openthinclient.web.ui;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.openthinclient.web.event.DashboardEvent;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.vaadin.spring.events.EventBus;
+
 import com.vaadin.event.ShortcutAction.KeyCode;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.server.Page;
 import com.vaadin.server.Responsive;
 import com.vaadin.shared.Position;
-import com.vaadin.ui.*;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CheckBox;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
-
-import org.openthinclient.web.event.DashboardEvent;
-import org.vaadin.spring.events.EventBus;
 
 /**
  * Full-screen UI component that allows the user to login.
@@ -20,6 +35,7 @@ import org.vaadin.spring.events.EventBus;
 public class LoginView extends VerticalLayout {
 
     private final EventBus.SessionEventBus eventBus;
+    private CheckBox rememberMe;
     
     public LoginView(EventBus.SessionEventBus eventBus) {
         this.eventBus = eventBus;
@@ -35,7 +51,7 @@ public class LoginView extends VerticalLayout {
         notification.setHtmlContentAllowed(true);
         notification.setStyleName("tray dark small closable login-help");
         notification.setPosition(Position.BOTTOM_CENTER);
-        notification.setDelayMsec(20000);
+        notification.setDelayMsec(10000);
         notification.show(Page.getCurrent());
 
     }
@@ -49,7 +65,18 @@ public class LoginView extends VerticalLayout {
 
         loginPanel.addComponent(buildLabels());
         loginPanel.addComponent(buildFields());
-        loginPanel.addComponent(new CheckBox("Remember me", true));
+        loginPanel.addComponent(rememberMe = new CheckBox("Remember me", false));
+        rememberMe.addValueChangeListener(event -> {
+          if (rememberMe.getValue()) {
+            Notification notification = new Notification("Remember me");
+            notification.setDescription("<span>Please note: to disable the always remember-me function you have to delete your cookies.");
+            notification.setHtmlContentAllowed(true);
+            notification.setStyleName("tray dark small closable login-help");
+            notification.setPosition(Position.BOTTOM_CENTER);
+            notification.setDelayMsec(10000);
+            notification.show(Page.getCurrent());
+          }
+        });
         return loginPanel;
     }
 
@@ -73,7 +100,7 @@ public class LoginView extends VerticalLayout {
         signin.addClickListener(new Button.ClickListener() {
         	@Override
         	public void buttonClick(final Button.ClickEvent event) {
-             eventBus.publish(this, new DashboardEvent.UserLoginRequestedEvent(username.getValue(), password.getValue()));
+             eventBus.publish(this, new DashboardEvent.UserLoginRequestedEvent(username.getValue(), password.getValue(), rememberMe.getValue()));
         	}
         });
 
