9d402e760d5c3888a92746b6b3baac005cfd1010
Francois Fernandes <f.fernandes@fernandes-it.de>
2016-05-02 21:01:02 +0200

    manager.home System property will take precedence
    This change will allow an installer to specify the actual manager home directory during the installation

--- services/common/src/main/java/org/openthinclient/service/common/home/impl/ManagerHomeFactory.java
+++ services/common/src/main/java/org/openthinclient/service/common/home/impl/ManagerHomeFactory.java
@@ -9,85 +9,115 @@ import java.util.prefs.Preferences;
 public class ManagerHomeFactory {
 
 
-  public static final String KEY_MANAGER_HOME = "manager-home";
+    public static final String KEY_MANAGER_HOME = "manager-home";
 
-  /**
-   * Checks if a manager home directory is known and if that directory seems to contain a valid installation.
-   * @return <code>true</code> if the manager home has been specified and seems to contain a valid installation.
-   */
-  public boolean isManagerHomeValidAndInstalled() {
-
-    final File managerHomeDirectory = getManagerHomeDirectory();
-    if (managerHomeDirectory == null)
-      return false;
-
-    // FIXME this is basically a duplication of the code in CheckManagerHomeDirectory
-
-    final File[] contents = managerHomeDirectory.listFiles(pathname -> {
-      // ignore typical MacOS directories
-      return !pathname.getName().equals(".DS_Store");
-    });
-
-    return contents != null && contents.length > 0;
+    /**
+     * Checks whether or not the manager home has been specified using a system property.
+     *
+     * @return <code>true</code> if the system property <code>manager.home</code> has been specified
+     */
+    public boolean isManagerHomeDefinedAsSystemProperty() {
+        return getManagerHomeSystemProperty() != null;
     }
 
-  /**
-   * Returns the manager home base directory or <code>null</code>.
-   * This method will only return a non-<code>null</code> value if either there has been a preconfigured manager home, or the <code>manager.home</code> system property has been set.
-   *
-   * @return a {@link File} pointing to the manager home directory or <code>null</code>
-   */
-  public File getManagerHomeDirectory() {
-
-    final Preferences preferences = getPreferences();
-
-    final String preconfiguredManagerHome = preferences.get(KEY_MANAGER_HOME, null);
-
-    if (preconfiguredManagerHome != null) {
-      return new File(preconfiguredManagerHome);
+    private String getManagerHomeSystemProperty() {
+        return System.getProperty("manager.home");
     }
 
-    // check whether there has been a system property set
-    final String value = System.getProperty("manager.home");
-
-    if (value != null && value.trim().length() > 0) {
-      return new File(value);
+    public boolean isManagerHomeDefinedAsPreference() {
+        return getManagerHomePreference() != null;
     }
 
-    return null;
-  }
+    /**
+     * Checks if a manager home directory is known and if that directory seems to contain a valid
+     * installation.
+     *
+     * @return <code>true</code> if the manager home has been specified and seems to contain a valid
+     * installation.
+     */
+    public boolean isManagerHomeValidAndInstalled() {
 
-  /**
-   * Specify the manager home directory. This setting will override the manager home system property, and the value will be persisted using {@link Preferences}.
-   *
-   * @param baseDirectory the base directory to be used as the new manager home.
-   */
-  public void setManagerHomeDirectory(File baseDirectory) {
-    if (baseDirectory == null) {
-      throw new IllegalArgumentException("baseDirectory must not be null");
-    }
-    final Preferences preferences = getPreferences();
-    preferences.put(KEY_MANAGER_HOME, baseDirectory.getAbsolutePath());
-    try {
-      preferences.flush();
-    } catch (BackingStoreException e) {
-      throw new RuntimeException("Failed to store manager home directory location.", e);
-    }
-  }
+        final File managerHomeDirectory = getManagerHomeDirectory();
+        if (managerHomeDirectory == null)
+            return false;
 
-  private Preferences getPreferences() {
-    return Preferences.userRoot().node("org").node("openthinclient").node("manager");
-  }
+        // FIXME this is basically a duplication of the code in CheckManagerHomeDirectory
 
-  public ManagerHome create() {
+        final File[] contents = managerHomeDirectory.listFiles(pathname -> {
+            return
+                    // ignore typical MacOS directories
+                    !pathname.getName().equals(".DS_Store") &&
+                            // the installer will create a system property logging.file which will point to a file in the logs directory.
+                            !pathname.getName().equals("logs");
+        });
 
-    final File managerHomeDirectory = getManagerHomeDirectory();
-
-    if (managerHomeDirectory == null) {
-      throw new IllegalStateException("No manager home directory has been specified");
+        return contents != null && contents.length > 0;
     }
 
-    return new DefaultManagerHome(managerHomeDirectory);
-  }
+    /**
+     * Returns the manager home base directory or <code>null</code>. This method will only return a
+     * non-<code>null</code> value if either there has been a preconfigured manager home, or the
+     * <code>manager.home</code> system property has been set.
+     *
+     * @return a {@link File} pointing to the manager home directory or <code>null</code>
+     */
+    public File getManagerHomeDirectory() {
+
+        // check whether there has been a system property set
+        final String value = getManagerHomeSystemProperty();
+
+        if (value != null && value.trim().length() > 0) {
+            return new File(value);
+        }
+
+
+        final String preconfiguredManagerHome = getManagerHomePreference();
+
+        if (preconfiguredManagerHome != null) {
+            return new File(preconfiguredManagerHome);
+        }
+
+        return null;
+    }
+
+    /**
+     * Specify the manager home directory. This value will be persisted using {@link Preferences}.
+     * Keep in mind, that the <code>manager.home</code> will take precedence in any case.
+     *
+     * @param baseDirectory the base directory to be used as the new manager home.
+     */
+    public void setManagerHomeDirectory(File baseDirectory) {
+        if (baseDirectory == null) {
+            throw new IllegalArgumentException("baseDirectory must not be null");
+        }
+        final Preferences preferences = getPreferences();
+        preferences.put(KEY_MANAGER_HOME, baseDirectory.getAbsolutePath());
+        try {
+            preferences.flush();
+        } catch (BackingStoreException e) {
+            throw new RuntimeException("Failed to store manager home directory location.", e);
+        }
+    }
+
+    private String getManagerHomePreference() {
+        final Preferences preferences = getPreferences();
+
+        return preferences.get(KEY_MANAGER_HOME, null);
+    }
+
+    private Preferences getPreferences() {
+        return Preferences.userRoot().node("org").node("openthinclient").node("manager");
+    }
+
+    public ManagerHome create() {
+
+        final File managerHomeDirectory = getManagerHomeDirectory();
+
+        if (managerHomeDirectory == null) {
+            throw new IllegalStateException("No manager home directory has been specified");
+        }
+
+        return new DefaultManagerHome(managerHomeDirectory);
+    }
 
 }
