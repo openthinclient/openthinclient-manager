2c1c1fd431376ed91258095d5ccb81fc5845de3c
Francois Fernandes <f.fernandes@fernandes-it.de>
2016-07-14 09:34:42 +0200

    Removed non required properties

--- console-web/api/src/main/java/org/openthinclient/api/rest/model/Device.java
+++ console-web/api/src/main/java/org/openthinclient/api/rest/model/Device.java
@@ -1,65 +1,8 @@
 package org.openthinclient.api.rest.model;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.validation.constraints.NotNull;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Device extends AbstractProfileObject {
-
-    /**
-     * (Required)
-     */
-    @JsonProperty("id")
-    @NotNull
-    private String id;
-    @JsonIgnore
-    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
-
-    /**
-     * (Required)
-     *
-     * @return The id
-     */
-    @JsonProperty("id")
-    public String getId() {
-        return id;
-    }
-
-    /**
-     * (Required)
-     *
-     * @param id The id
-     */
-    @JsonProperty("id")
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Device withId(String id) {
-        this.id = id;
-        return this;
-    }
-
-    @JsonAnyGetter
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    public void setAdditionalProperties(Map<String, Object> additionalProperties) {
-        this.additionalProperties = additionalProperties;
-    }
-
-    @JsonAnySetter
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
 
 }
