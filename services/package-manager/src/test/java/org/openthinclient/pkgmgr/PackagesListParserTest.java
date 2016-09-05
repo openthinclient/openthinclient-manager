2a78b5dcfdf1b9f40b551551afbcafaa4384161d
Joerg Neumann <neumann.jorg@googlemail.com>
2016-09-05 23:14:52 +0200

    SOFTWARE-351 Merge fixed Packages

--- services/package-manager/src/test/java/org/openthinclient/pkgmgr/PackagesListParserTest.java
+++ services/package-manager/src/test/java/org/openthinclient/pkgmgr/PackagesListParserTest.java
@@ -1,55 +1,46 @@
 package org.openthinclient.pkgmgr;
 
-import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.Test;
+import org.openthinclient.pkgmgr.db.Package;
+import org.openthinclient.pkgmgr.db.Version;
 import org.openthinclient.util.dpkg.PackageReferenceListParser;
 import org.openthinclient.util.dpkg.PackagesListParser;
-
-import java.io.InputStream;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 public class PackagesListParserTest {
 
   @Test
-  @Ignore("Order has changed")
   public void testPackagesParsing() throws Exception {
     
     final List<org.openthinclient.pkgmgr.db.Package> packageList = new PackagesListParser()
             .parse(getClass().getResourceAsStream("/test-repository/Packages"));
 
-    assertEquals(21, packageList.size());
+    assertEquals(20, packageList.size());
 
-    assertEquals("foo", packageList.get(0).getName());
-    assertEquals("zonk", packageList.get(5).getName());
-    assertEquals(new PackageReferenceListParser().parse("bar2"), packageList.get(5).getConflicts());
+    Package zonk = getPackage(packageList, "zonk", "2.0-1");
+    assertEquals(new PackageReferenceListParser().parse("bar2"), zonk.getConflicts());
 
-    assertEquals("bar2", packageList.get(9).getName());
-    assertEquals(new PackageReferenceListParser().parse("foo"), packageList.get(9).getDepends());
-    assertEquals("bar", packageList.get(8).getName());
-    assertEquals(new PackageReferenceListParser().parse("foo (>= 0:2.1-1)"), packageList.get(8).getDepends());
-
-  }
-
-  @Test
-  @Ignore("Order has changed")
-  public void testParsePackages() throws Exception {
-
-    final InputStream packagesStream = getClass().getResourceAsStream("/test-repository/Packages");
-    assertNotNull(packagesStream);
-
-    final PackagesListParser factory = new PackagesListParser();
-    final List<org.openthinclient.pkgmgr.db.Package> packages = factory.parse(packagesStream);
-
-    assertEquals(21, packages.size());
-
-    assertEquals("foo", packages.get(0).getName());
-    assertEquals("zonk", packages.get(5).getName());
-    assertEquals("bar2", packages.get(9).getName());
-    assertEquals("bar", packages.get(8).getName());
-
+    Package bar2 = getPackage(packageList, "bar2", "2.0-1");
+    assertEquals(new PackageReferenceListParser().parse("foo"), bar2.getDepends());
+    
+    Package bar = getPackage(packageList, "bar", "2.0-1");
+    assertEquals(new PackageReferenceListParser().parse("foo (>= 0:2.1-1)"), bar.getDepends());
 
   }
+
+  private Package getPackage(List<Package> packages, String name, String version) throws Exception {
+     Optional<Package> findAny = packages.stream()
+                                         .filter(p -> p.getName().equals(name) && p.getVersion().equals(Version.parse(version)))
+                                         .findAny();
+     if (findAny.isPresent()) {
+       return findAny.get();
+     } else {
+       throw new Exception("Package " + name + " with version " + version + " not found.");
+     }
+  }
+  
 }
