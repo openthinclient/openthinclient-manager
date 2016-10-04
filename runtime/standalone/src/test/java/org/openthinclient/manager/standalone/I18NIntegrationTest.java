3202ce3929d6225f5b4e7537a8429f9531fbd62f
Francois Fernandes <f.fernandes@fernandes-it.de>
2016-10-04 11:00:46 +0200

    Removing all transitive dependencies for console desktop

---
+++ runtime/standalone/src/test/java/org/openthinclient/manager/standalone/I18NIntegrationTest.java
@@ -0,0 +1,41 @@
+package org.openthinclient.manager.standalone;
+
+import org.junit.Test;
+import org.openthinclient.advisor.AdvisorMessages;
+import org.openthinclient.web.i18n.ConsoleWebMessages;
+import org.openthinclient.wizard.FirstStartWizardMessages;
+
+import java.util.List;
+
+import ch.qos.cal10n.verifier.Cal10nError;
+import ch.qos.cal10n.verifier.IMessageKeyVerifier;
+import ch.qos.cal10n.verifier.MessageKeyVerifier;
+
+import static org.junit.Assert.assertEquals;
+
+public class I18NIntegrationTest {
+
+  @Test
+  public void testFirstStartWizardMessages() {
+    assertMessagesCorrect(FirstStartWizardMessages.class);
+  }
+
+  @Test
+  public void testConsoleWebMessages() throws Exception {
+    assertMessagesCorrect(ConsoleWebMessages.class);
+  }
+
+  @Test
+  public void testAdvisorMessages() throws Exception {
+    assertMessagesCorrect(AdvisorMessages.class);
+  }
+
+  private void assertMessagesCorrect(Class<? extends Enum<?>> clazz) {
+    IMessageKeyVerifier mkv = new MessageKeyVerifier(clazz);
+    List<Cal10nError> errorList = mkv.verifyAllLocales();
+    for (Cal10nError error : errorList) {
+      System.out.println(error);
+    }
+    assertEquals(0, errorList.size());
+  }
+}
