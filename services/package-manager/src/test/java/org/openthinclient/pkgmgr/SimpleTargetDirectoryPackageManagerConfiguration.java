3db788af827b1b6b6fb03dd07886ca4d3ab6af5c
Francois Fernandes <f.fernandes@fernandes-it.de>
2016-10-10 19:20:53 +0200

    Switched to NIO to force exceptions
    The old File API will not throw Exceptions if something goes wrong. Switched to NIO, which will in case of an error report details.

--- services/package-manager/src/test/java/org/openthinclient/pkgmgr/SimpleTargetDirectoryPackageManagerConfiguration.java
+++ services/package-manager/src/test/java/org/openthinclient/pkgmgr/SimpleTargetDirectoryPackageManagerConfiguration.java
@@ -1,6 +1,5 @@
 package org.openthinclient.pkgmgr;
 
-import org.junit.Assert;
 import org.openthinclient.pkgmgr.spring.PackageManagerExecutionEngineConfiguration;
 import org.openthinclient.pkgmgr.spring.PackageManagerFactoryConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,69 +9,73 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Scope;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertTrue;
 
 @Configuration
 @Import({PackageManagerInMemoryDatabaseConfiguration.class, PackageManagerExecutionEngineConfiguration.class, PackageManagerFactoryConfiguration.class})
 public class SimpleTargetDirectoryPackageManagerConfiguration {
 
-    @Autowired
-    PackageManagerFactory packageManagerFactory;
+  @Autowired
+  PackageManagerFactory packageManagerFactory;
 
-    @Bean
-    @Scope(value = "prototype")
-    public File targetDirectory() {
-        File targetDirectory = new File("target");
+  @Bean
+  @Scope(value = "prototype")
+  public File targetDirectory() throws Exception {
+    Path targetDirectory = Paths.get("target");
 
-        if (!targetDirectory.exists()) {
-            Assert.assertTrue(targetDirectory.mkdirs());
-        }
-
-        if (!targetDirectory.isDirectory())
-            throw new AssertionError("Expected: " + targetDirectory.getAbsolutePath() + " to be a directory");
-
-        // we're creating an subdirectory per test, to ensure that there will be no other existing files
-        targetDirectory = new File(targetDirectory, "pkgmgr-test-" + System.currentTimeMillis());
-        Assert.assertTrue(targetDirectory.mkdirs());
-
-        return targetDirectory;
+    if (!Files.exists(targetDirectory)) {
+      Files.createDirectories(targetDirectory);
     }
 
-    @Bean
-    @Scope(value = "prototype")
-    public PackageManagerConfiguration packageManagerConfiguration() throws Exception {
+    assertTrue("Expected " + targetDirectory + " to be a directory", Files.isDirectory(targetDirectory));
+    // we're creating an subdirectory per test, to ensure that there will be no other existing files
+    targetDirectory = targetDirectory.resolve("pkgmgr-test-" + System.currentTimeMillis());
 
-        File targetDirectory = targetDirectory();
-        final PackageManagerConfiguration configuration = new PackageManagerConfiguration();
+    Files.createDirectories(targetDirectory);
 
-        configuration.setInstallDir(dir(targetDirectory, "install"));
-        configuration.setWorkingDir(dir(targetDirectory, "cache"));
-        configuration.setArchivesDir(dir(targetDirectory, "archives"));
-        configuration.setTestinstallDir(dir(targetDirectory, "install-test"));
-        configuration.setPartialDir(dir(targetDirectory, "archives-partial"));
-        configuration.setInstallOldDir(dir(targetDirectory, "install-old"));
-        configuration.setListsDir(dir(targetDirectory, "lists"));
+    return targetDirectory.toFile();
+  }
 
-        final File dbDir = dir(targetDirectory, "db");
+  @Bean
+  @Scope(value = "prototype")
+  public PackageManagerConfiguration packageManagerConfiguration() throws Exception {
 
-        configuration.setPackageDB(new File(dbDir, "package.db"));
-        configuration.setCacheDB(new File(dbDir, "chache.db"));
-        configuration.setOldDB(new File(dbDir, "remove.db"));
-        configuration.setArchivesDB(new File(dbDir, "archives.db"));
+    File targetDirectory = targetDirectory();
+    final PackageManagerConfiguration configuration = new PackageManagerConfiguration();
 
-        return configuration;
+    configuration.setInstallDir(dir(targetDirectory, "install"));
+    configuration.setWorkingDir(dir(targetDirectory, "cache"));
+    configuration.setArchivesDir(dir(targetDirectory, "archives"));
+    configuration.setTestinstallDir(dir(targetDirectory, "install-test"));
+    configuration.setPartialDir(dir(targetDirectory, "archives-partial"));
+    configuration.setInstallOldDir(dir(targetDirectory, "install-old"));
+    configuration.setListsDir(dir(targetDirectory, "lists"));
 
-    }
+    final File dbDir = dir(targetDirectory, "db");
 
-    @Bean
-    @Scope(value="prototype")
-    public PackageManager packageManager() throws Exception {
-        return packageManagerFactory.createPackageManager(packageManagerConfiguration());
-    }
+    configuration.setPackageDB(new File(dbDir, "package.db"));
+    configuration.setCacheDB(new File(dbDir, "chache.db"));
+    configuration.setOldDB(new File(dbDir, "remove.db"));
+    configuration.setArchivesDB(new File(dbDir, "archives.db"));
 
-    private File dir(File targetDirectory, String subPath) {
-        final File dir = new File(targetDirectory, subPath);
-        Assert.assertTrue(dir.mkdirs());
-        return dir;
-    }
+    return configuration;
+
+  }
+
+  @Bean
+  @Scope(value = "prototype")
+  public PackageManager packageManager() throws Exception {
+    return packageManagerFactory.createPackageManager(packageManagerConfiguration());
+  }
+
+  private File dir(File targetDirectory, String subPath) {
+    final File dir = new File(targetDirectory, subPath);
+    assertTrue(dir.mkdirs());
+    return dir;
+  }
 
 }
