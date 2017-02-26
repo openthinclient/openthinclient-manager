a9d0787191cb92998397f86fabe8744f10649a7d
Francois Fernandes <f.fernandes@fernandes-it.de>
2017-02-26 13:38:33 +0100

    Separated the configuration for the LDAP services to allow easier reuse

--- runtime/standalone/src/main/java/org/openthinclient/manager/standalone/config/DirectoryServicesConfiguration.java
+++ runtime/standalone/src/main/java/org/openthinclient/manager/standalone/config/DirectoryServicesConfiguration.java
@@ -3,22 +3,6 @@ package org.openthinclient.manager.standalone.config;
 import org.openthinclient.common.model.schema.provider.AbstractSchemaProvider;
 import org.openthinclient.common.model.schema.provider.SchemaProvider;
 import org.openthinclient.common.model.schema.provider.ServerLocalSchemaProvider;
-import org.openthinclient.common.model.service.ApplicationService;
-import org.openthinclient.common.model.service.ClientService;
-import org.openthinclient.common.model.service.DefaultLDAPApplicationService;
-import org.openthinclient.common.model.service.DefaultLDAPClientService;
-import org.openthinclient.common.model.service.DefaultLDAPDeviceService;
-import org.openthinclient.common.model.service.DefaultLDAPHardwareTypeService;
-import org.openthinclient.common.model.service.DefaultLDAPLocationService;
-import org.openthinclient.common.model.service.DefaultLDAPPrinterService;
-import org.openthinclient.common.model.service.DefaultLDAPRealmService;
-import org.openthinclient.common.model.service.DefaultLDAPUnrecognizedClientService;
-import org.openthinclient.common.model.service.DeviceService;
-import org.openthinclient.common.model.service.HardwareTypeService;
-import org.openthinclient.common.model.service.LocationService;
-import org.openthinclient.common.model.service.PrinterService;
-import org.openthinclient.common.model.service.RealmService;
-import org.openthinclient.common.model.service.UnrecognizedClientService;
 import org.openthinclient.ldap.LDAPConnectionDescriptor;
 import org.openthinclient.ldap.auth.UsernamePasswordHandler;
 import org.openthinclient.service.apacheds.DirectoryServiceConfiguration;
@@ -26,10 +10,12 @@ import org.openthinclient.service.common.home.ManagerHome;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import java.io.File;
 
 @Configuration
+@Import(LDAPServicesConfiguration.class)
 public class DirectoryServicesConfiguration {
 
   @Autowired
@@ -50,11 +36,6 @@ public class DirectoryServicesConfiguration {
   }
 
   @Bean
-  public RealmService realmService() {
-    return new DefaultLDAPRealmService(schemaProvider(), ldapConnectionDescriptor());
-  }
-
-  @Bean
   public SchemaProvider schemaProvider() {
     final File homeDirectory = managerHome.getLocation();
 
@@ -64,38 +45,4 @@ public class DirectoryServicesConfiguration {
 
   }
 
-  @Bean
-  public ClientService clientService() {
-    return new DefaultLDAPClientService(realmService());
-  }
-
-  @Bean
-  public UnrecognizedClientService unrecognizedClientService() {
-    return new DefaultLDAPUnrecognizedClientService(realmService());
-  }
-
-  @Bean
-  public ApplicationService applicationService() {
-    return new DefaultLDAPApplicationService(realmService());
-  }
-
-  @Bean
-  public DeviceService deviceService() {
-    return new DefaultLDAPDeviceService(realmService());
-  }
-
-  @Bean
-  public HardwareTypeService hardwareTypeService() {
-    return new DefaultLDAPHardwareTypeService(realmService());
-  }
-
-  @Bean
-  public LocationService locationService() {
-    return new DefaultLDAPLocationService(realmService());
-  }
-
-  @Bean
-  public PrinterService printerService() {
-    return new DefaultLDAPPrinterService(realmService());
-  }
 }
