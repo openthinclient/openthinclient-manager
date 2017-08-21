5ae781b35ca9070fead9b820c87d98542c1e84a5
Joerg Neumann <neumann.jorg@googlemail.com>
2017-08-21 22:26:55 +0200

    SOFTWARE-514 Logging error message

--- console-web/webapp/src/main/java/org/openthinclient/web/ui/ViewHeader.java
+++ console-web/webapp/src/main/java/org/openthinclient/web/ui/ViewHeader.java
@@ -2,10 +2,17 @@ package org.openthinclient.web.ui;
 
 import ch.qos.cal10n.IMessageConveyor;
 import ch.qos.cal10n.MessageConveyor;
+import com.vaadin.server.DefaultErrorHandler;
+import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
+import org.openthinclient.web.i18n.ConsoleWebMessages;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ViewHeader extends VerticalLayout {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ViewHeader.class);
 
   public static final String TITLE_ID = "dashboard-title";
   private final HorizontalLayout tools;
@@ -41,15 +48,16 @@ public class ViewHeader extends VerticalLayout {
     }
 
     // Configure the error handler for the UI
-//    UI.getCurrent().setErrorHandler(new DefaultErrorHandler() {
-//      @Override
-//      public void error(com.vaadin.server.ErrorEvent event) {
-//        // Display the error message in a custom fashion
-//        Label errorMessage = new Label(mc.getMessage(ConsoleWebMessages.UI_UNEXPECTED_ERROR), ContentMode.HTML);
-//        errorMessage.addStyleName("unexpected_error");
-//        addComponent(errorMessage);
-//      }
-//    });
+    UI.getCurrent().setErrorHandler(new DefaultErrorHandler() {
+      @Override
+      public void error(com.vaadin.server.ErrorEvent event) {
+        LOGGER.error("Caught unexpected error.", event.getThrowable());
+        // Display the error message in a custom fashion
+        Label errorMessage = new Label(mc.getMessage(ConsoleWebMessages.UI_UNEXPECTED_ERROR), ContentMode.HTML);
+        errorMessage.addStyleName("unexpected_error");
+        addComponent(errorMessage);
+      }
+    });
   }
 
   public ViewHeader(String title) {
