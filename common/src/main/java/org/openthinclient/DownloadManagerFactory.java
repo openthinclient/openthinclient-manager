5e8fb3cf94729b747e58e2990b861c428152fb33
Joerg Neumann <neumann.jorg@googlemail.com>
2017-10-19 21:12:36 +0200

    SOFTWARE-542 logger added

--- common/src/main/java/org/openthinclient/DownloadManagerFactory.java
+++ common/src/main/java/org/openthinclient/DownloadManagerFactory.java
@@ -3,6 +3,8 @@ package org.openthinclient;
 import org.openthinclient.manager.util.http.DownloadManager;
 import org.openthinclient.manager.util.http.config.NetworkConfiguration;
 import org.openthinclient.manager.util.http.impl.HttpClientDownloadManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
 import java.util.Properties;
@@ -11,6 +13,8 @@ import java.util.Properties;
  * DownloadManagerFactory
  */
 public class DownloadManagerFactory {
+
+    private static final Logger logger = LoggerFactory.getLogger(DownloadManagerFactory.class);
 
     public static DownloadManager create(String serverID, NetworkConfiguration.ProxyConfiguration proxyConfiguration) {
 
@@ -21,7 +25,7 @@ public class DownloadManagerFactory {
             p.load(inputStream);
             version = p.getProperty("version");
         } catch (Exception e) {
-            System.err.println("Cannot read version from pom.properties.");
+            logger.error("Cannot read version from pom.properties.", e);
         }
 
         String userAgent = version == null ? serverID : serverID + "-" + version;
