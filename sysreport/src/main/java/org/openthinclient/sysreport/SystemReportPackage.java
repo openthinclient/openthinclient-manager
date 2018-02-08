bc9bf8e36d015c0fec85357f9d4215e3bb45853d
Francois Fernandes <f.fernandes@fernandes-it.de>
2018-02-08 20:59:44 +0100

    SOFTWARE-628 Additional content for the report file

--- sysreport/src/main/java/org/openthinclient/sysreport/SystemReportPackage.java
+++ sysreport/src/main/java/org/openthinclient/sysreport/SystemReportPackage.java
@@ -1,9 +1,11 @@
 package org.openthinclient.sysreport;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -15,7 +17,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Supplier;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -26,16 +27,19 @@ import java.util.zip.ZipOutputStream;
 public class SystemReportPackage {
 
   public static final String PATH_REPORTS = "tcos-reports/";
+  private static final String PATH_LOGS = "logs/";
   private final SystemReport report;
   /**
    * A {@link Map} containing references to report files, by client MAC address. The key in the
    * {@link Map} is the MAC address for a specific client.
    */
   private final Map<String, List<Path>> tcosReports;
+  private final List<Path> logfiles;
 
   public SystemReportPackage(SystemReport report) {
     this.report = report;
     tcosReports = new HashMap<>();
+    logfiles = new ArrayList<>();
   }
 
   /**
@@ -53,10 +57,16 @@ public class SystemReportPackage {
     paths.add(reportFile);
   }
 
+  public void addLogfile(Path logfile) {
+    if (!Files.isRegularFile(logfile))
+      throw new IllegalArgumentException("Provided path does not point to a valid file: " + logfile);
 
-  public void save(Supplier<OutputStream> outputSupplier) throws IOException {
+    logfiles.add(logfile);
+  }
 
-    try (final OutputStream out = outputSupplier.get(); final ZipOutputStream zip = new ZipOutputStream(out)) {
+  public void save(OutputStream out) throws IOException {
+
+    try (final ZipOutputStream zip = new ZipOutputStream(out)) {
 
       final Instant creationTimestamp = Instant.now();
       // we're ok if the compression actually takes a while. Save some space during transmission
@@ -67,13 +77,33 @@ public class SystemReportPackage {
 
       writeTcosReports(zip, creationTimestamp);
 
+      writeLogfiles(zip, creationTimestamp);
 
+      zip.finish();
     }
 
   }
 
+  private void writeLogfiles(ZipOutputStream zip, Instant creationTimestamp) throws IOException {
+    zip.putNextEntry(new ZipEntry(PATH_LOGS));
+    zip.closeEntry();
+
+    for (Path logfile : logfiles) {
+      writeLogfile(zip, logfile, creationTimestamp);
+    }
+  }
+
+  private void writeLogfile(ZipOutputStream zip, Path logfile, Instant creationTimestamp) throws IOException {
+    final ZipEntry entry = new ZipEntry(PATH_LOGS + logfile.getFileName().toString());
+    entry.setCreationTime(Files.getLastModifiedTime(logfile));
+    zip.putNextEntry(entry);
+    Files.copy(logfile, zip);
+    zip.closeEntry();
+  }
+
   private void writeTcosReports(ZipOutputStream zip, Instant creationTimestamp) throws IOException {
     zip.putNextEntry(new ZipEntry(PATH_REPORTS));
+    zip.closeEntry();
 
     for (Map.Entry<String, List<Path>> e : tcosReports.entrySet()) {
       writeTcosReports(zip, e.getKey(), e.getValue(), creationTimestamp);
@@ -81,8 +111,8 @@ public class SystemReportPackage {
   }
 
   private void writeTcosReports(ZipOutputStream zip, String clientMacAddress, List<Path> tcosReports, Instant creationTimestamp) throws IOException {
-
     zip.putNextEntry(new ZipEntry(PATH_REPORTS + clientMacAddress+"/"));
+    zip.closeEntry();
 
     for (Path tcosReport : tcosReports) {
       writeTcosReport(zip, clientMacAddress, tcosReport, creationTimestamp);
@@ -90,23 +120,31 @@ public class SystemReportPackage {
   }
 
   private void writeTcosReport(ZipOutputStream zip, String clientMacAddress, Path tcosReport, Instant creationTimestamp) throws IOException {
-    zip.putNextEntry(new ZipEntry(PATH_REPORTS + clientMacAddress + "/" + tcosReport.getFileName().toString()));
+    final ZipEntry entry = new ZipEntry(PATH_REPORTS + clientMacAddress + "/" + tcosReport.getFileName().toString());
+    entry.setCreationTime(Files.getLastModifiedTime(tcosReport));
+    zip.putNextEntry(entry);
     Files.copy(tcosReport, zip);
+    zip.closeEntry();
   }
 
   private void writeSystemReport(ZipOutputStream zip, Instant creationTimestamp) throws IOException {
-
     final ZipEntry entry = new ZipEntry("system-report.json");
-    entry.setComment("Main report file");
     entry.setCreationTime(FileTime.from(creationTimestamp));
+    zip.putNextEntry(entry);
 
     final ObjectMapper mapper = new ObjectMapper();
     // prevent the ObjectWriter to close the stream
-    mapper.disable(SerializationFeature.CLOSE_CLOSEABLE);
+    mapper.disable(SerializationFeature.CLOSE_CLOSEABLE)
+    .disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
     final ObjectWriter writer = mapper.writer();
 
-    writer.writeValue(zip, report);
+    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    writer.writeValue(baos, report);
 
+    zip.write(baos.toByteArray());
+
+    zip.flush();
+    zip.closeEntry();
   }
 
 
