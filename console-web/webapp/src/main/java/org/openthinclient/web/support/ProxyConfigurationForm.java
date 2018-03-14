7e8f0e2901d7a0ec54a632a1fd0257863cf9b20b
Joerg Neumann <neumann.jorg@googlemail.com>
2018-03-14 23:17:42 +0100

    MANAGER-137 Initial proxy configuration

---
+++ console-web/webapp/src/main/java/org/openthinclient/web/support/ProxyConfigurationForm.java
@@ -0,0 +1,134 @@
+package org.openthinclient.web.support;
+
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_FIRSTSTART_INSTALLSTEPS_CONFIGURENETWORKSTEP_PROXY_CONNECTION_AUTH;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_FIRSTSTART_INSTALLSTEPS_CONFIGURENETWORKSTEP_PROXY_CONNECTION_HOST_INVALID;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_FIRSTSTART_INSTALLSTEPS_CONFIGURENETWORKSTEP_PROXY_CONNECTION_HOST_MISSING;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_FIRSTSTART_INSTALLSTEPS_CONFIGURENETWORKSTEP_PROXY_CONNECTION_PORT_INVALID;
+
+import ch.qos.cal10n.IMessageConveyor;
+import ch.qos.cal10n.MessageConveyor;
+import com.vaadin.data.Binder;
+import com.vaadin.data.converter.StringToIntegerConverter;
+import com.vaadin.data.validator.IntegerRangeValidator;
+import com.vaadin.data.validator.StringLengthValidator;
+import com.vaadin.ui.CheckBox;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+import org.openthinclient.manager.util.http.config.NetworkConfiguration;
+import org.openthinclient.manager.util.http.config.NetworkConfiguration.ProxyConfiguration;
+
+public class ProxyConfigurationForm extends CustomComponent {
+
+  /** serialVersionUID */
+  private static final long serialVersionUID = -120512201002490319L;
+  
+  private final TextField hostField;
+  private final TextField portField;
+  private final TextField userField;
+  private final PasswordField passwordField;
+  private final CheckBox authenticationCheckbox;
+//  private final FieldGroup fieldGroup;
+  private final Binder<NetworkConfiguration.ProxyConfiguration> binder;
+
+  private final ProxyConfiguration proxyConfiguration;
+
+  public ProxyConfigurationForm(ProxyConfiguration proxyConfiguration) {
+    
+    IMessageConveyor mc = new MessageConveyor(UI.getCurrent().getLocale());
+
+    this.proxyConfiguration = proxyConfiguration;
+
+    authenticationCheckbox = new CheckBox(mc.getMessage(UI_FIRSTSTART_INSTALLSTEPS_CONFIGURENETWORKSTEP_PROXY_CONNECTION_AUTH));
+//    this.fieldGroup = new FieldGroup(networkConfigurationModel.getProxyConfigurationItem());
+
+    this.binder = new Binder<>();
+
+    userField = new TextField("Username");
+    userField.setPlaceholder("Username");
+    passwordField = new PasswordField("Password");
+    passwordField.setPlaceholder("Password");
+
+    this.binder.forField(userField) //
+            .withNullRepresentation("") // empty string instead of "null"
+            .bind(NetworkConfiguration.ProxyConfiguration::getUser, NetworkConfiguration.ProxyConfiguration::setUser);
+    this.binder.forField(passwordField) //
+            .withNullRepresentation("") //
+            .bind(NetworkConfiguration.ProxyConfiguration::getPassword, NetworkConfiguration.ProxyConfiguration::setPassword);
+
+    // hostField
+    hostField = new TextField("Hostname");
+    hostField.setPlaceholder("proxy.example.com");
+    this.binder.forField(hostField)
+            .withValidator(new StringLengthValidator(mc.getMessage(UI_FIRSTSTART_INSTALLSTEPS_CONFIGURENETWORKSTEP_PROXY_CONNECTION_HOST_MISSING), 1, null))
+            .withValidator(new HostnameValidator(mc.getMessage(UI_FIRSTSTART_INSTALLSTEPS_CONFIGURENETWORKSTEP_PROXY_CONNECTION_HOST_INVALID)))
+            .withNullRepresentation("")
+            .bind(NetworkConfiguration.ProxyConfiguration::getHost, NetworkConfiguration.ProxyConfiguration::setHost);
+
+    // portField with converter to NOT use thousand separator on 'port'-field
+    portField = new TextField("Port");
+    this.binder.forField(portField)
+               .withConverter(new StringToIntegerConverter(mc.getMessage(UI_FIRSTSTART_INSTALLSTEPS_CONFIGURENETWORKSTEP_PROXY_CONNECTION_PORT_INVALID)) {
+                  private static final long serialVersionUID = -6464686484330572080L;
+                  @Override
+                  protected NumberFormat getFormat(Locale locale) {
+                    // do not use a thousands separator, as HTML5 input type
+                    // number expects a fixed wire/DOM number format regardless
+                    // of how the browser presents it to the user (which could
+                    // depend on the browser locale)
+                    DecimalFormat format = new DecimalFormat();
+                    format.setMaximumFractionDigits(0);
+                    format.setDecimalSeparatorAlwaysShown(false);
+                    format.setParseIntegerOnly(true);
+                    format.setGroupingUsed(false);
+                    return format;
+                  }
+                })
+              .withValidator(new IntegerRangeValidator(mc.getMessage(UI_FIRSTSTART_INSTALLSTEPS_CONFIGURENETWORKSTEP_PROXY_CONNECTION_PORT_INVALID), 1, 65535))
+              .bind(NetworkConfiguration.ProxyConfiguration::getPort, NetworkConfiguration.ProxyConfiguration::setPort);
+
+    // read the bean last, as it will only update bindings that already exist.
+    this.binder.readBean(proxyConfiguration);
+
+    final FormLayout form = new FormLayout();
+    form.addComponent(hostField);
+    form.addComponent(portField);
+    form.addComponent(authenticationCheckbox);
+    form.addComponent(userField);
+    form.addComponent(passwordField);
+
+    authenticationCheckbox.addValueChangeListener(e -> {
+      updateEnabledState();
+    });
+
+    updateEnabledState();
+
+    setCompositionRoot(form);
+
+  }
+
+  private void updateEnabledState() {
+
+    hostField.setEnabled(isEnabled());
+    portField.setEnabled(isEnabled());
+    boolean userEditEnabled = isEnabled() && authenticationCheckbox.getValue();
+    userField.setEnabled(userEditEnabled);
+    passwordField.setEnabled(userEditEnabled);
+  }
+
+  @Override
+  public void setEnabled(boolean enabled) {
+    super.setEnabled(enabled);
+    updateEnabledState();
+  }
+
+  public void commit() {
+    binder.writeBeanIfValid(proxyConfiguration);
+  }
+
+}
---
+++ console-web/webapp/src/main/java/org/openthinclient/web/support/ProxyConfigurationView.java
@@ -0,0 +1,117 @@
+package org.openthinclient.web.support;
+
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_APPLICATION_HEADER;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_APPLICATION_UPDATE_EXIT;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_APPLICATION_UPDATE_RUNNING;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_APPLICATION_UPDATE_SUCCESS;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_CHECK_APPLICATION_VERSION_NOTIFICATION_CAPTION;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_CHECK_APPLICATION_VERSION_NOTIFICATION_FAIL;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_CHECK_APPLICATION_VERSION_NOTIFICATION_OK;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_CHECK_APPLICATION_VERSION_NOTIFICATION_UPDATE;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_CONSOLE_ABOUT_HEADER;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_PROXY_CONFIGURATION_HEADER;
+
+import ch.qos.cal10n.MessageConveyor;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.server.Responsive;
+import com.vaadin.shared.ui.ContentMode;
+import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.ProgressBar;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+import java.net.URI;
+import javax.annotation.PostConstruct;
+import org.openthinclient.api.proc.RuntimeProcessExecutor;
+import org.openthinclient.api.versioncheck.AvailableVersionChecker;
+import org.openthinclient.api.versioncheck.UpdateDescriptor;
+import org.openthinclient.manager.util.http.DownloadManager;
+import org.openthinclient.manager.util.http.config.NetworkConfiguration;
+import org.openthinclient.manager.util.http.config.NetworkConfiguration.ProxyConfiguration;
+import org.openthinclient.pkgmgr.PackageManagerConfiguration;
+import org.openthinclient.pkgmgr.db.Version;
+import org.openthinclient.progress.NoopProgressReceiver;
+import org.openthinclient.service.common.home.Configuration;
+import org.openthinclient.service.common.home.ManagerHome;
+import org.openthinclient.web.component.NotificationDialog;
+import org.openthinclient.web.event.DashboardEventBus;
+import org.openthinclient.web.i18n.ConsoleWebMessages;
+import org.openthinclient.web.ui.ViewHeader;
+import org.openthinclient.web.view.DashboardSections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.vaadin.spring.sidebar.annotation.SideBarItem;
+
+@SpringView(name = "proxy-config")
+@SideBarItem(sectionId = DashboardSections.SUPPORT, captionCode = "UI_SUPPORT_PROXY_CONFIGURATION_HEADER", order = 10)
+public class ProxyConfigurationView extends Panel implements View {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ProxyConfigurationView.class);
+
+  @Autowired
+  private ManagerHome managerHome;
+
+  final MessageConveyor mc;
+  final VerticalLayout root ;
+
+  public ProxyConfigurationView() {
+
+     mc = new MessageConveyor(UI.getCurrent().getLocale());
+     
+     addStyleName(ValoTheme.PANEL_BORDERLESS);
+     setSizeFull();
+     DashboardEventBus.register(this);
+
+     root = new VerticalLayout();
+     root.setSizeFull();
+     root.setMargin(true);
+     root.addStyleName("dashboard-view");
+     setContent(root);
+     Responsive.makeResponsive(root);
+
+     root.addComponent(new ViewHeader(mc.getMessage(UI_SUPPORT_PROXY_CONFIGURATION_HEADER)));
+  }
+
+  @Override
+  public String getCaption() {
+     return mc.getMessage(UI_SUPPORT_PROXY_CONFIGURATION_HEADER);
+  }
+
+  @PostConstruct
+  private void init() {
+
+      buildContent();
+  }
+  
+  private void buildContent() {
+
+    VerticalLayout content = new VerticalLayout();
+     
+     final Label labelDescription = new Label(mc.getMessage(ConsoleWebMessages.UI_SUPPORT_PROXY_CONFIGURATION_DESCRIPTION), ContentMode.HTML);
+     content.addComponent(labelDescription);
+
+    PackageManagerConfiguration configuration = managerHome.getConfiguration(PackageManagerConfiguration.class);
+    ProxyConfiguration proxyConfiguration = configuration.getProxyConfiguration();
+
+    Panel p = new Panel();
+    p.setContent(new ProxyConfigurationForm(proxyConfiguration));
+
+    content.addComponent(p);
+
+    root.addComponent(content);
+     root.setExpandRatio(content, 1);
+
+  }
+
+  @Override
+  public void enter(ViewChangeListener.ViewChangeEvent event) {
+
+  }
+
+}
