a51845adc689404314307d291c98863f9f53aee7
Francois Fernandes <f.fernandes@fernandes-it.de>
2018-09-30 23:38:39 +0200

    MANAGER-283 disabled system report view

--- console-web/webapp/src/main/java/org/openthinclient/web/support/SystemReportView.java
+++ console-web/webapp/src/main/java/org/openthinclient/web/support/SystemReportView.java
@@ -1,26 +1,20 @@
 package org.openthinclient.web.support;
 
+import ch.qos.cal10n.IMessageConveyor;
+import ch.qos.cal10n.MessageConveyor;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.Responsive;
-import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.UI;
-
 import org.openthinclient.sysreport.SystemReport;
 import org.openthinclient.web.support.ui.SystemReportDesign;
-import org.openthinclient.web.view.DashboardSections;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.vaadin.spring.sidebar.annotation.SideBarItem;
 
-import ch.qos.cal10n.IMessageConveyor;
-import ch.qos.cal10n.MessageConveyor;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.*;
 
-import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_SYSTEMREPORT_CAPTION;
-import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_SYSTEMREPORT_DESCRIPTION;
-import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_SYSTEMREPORT_TRANSMITTED;
-
-@SpringView(name = "system-report")
-@SideBarItem(sectionId = DashboardSections.SUPPORT, captionCode = "UI_SUPPORT_SYSTEMREPORT_CAPTION")
+// FIXME Disabled due to MANAGER-283 having a higher priority. Re-enable once the system report is required
+//@SpringView(name = "system-report")
+//@SideBarItem(sectionId = DashboardSections.SUPPORT, captionCode = "UI_SUPPORT_SYSTEMREPORT_CAPTION")
 public class SystemReportView extends SystemReportDesign implements View {
 
   @Autowired
