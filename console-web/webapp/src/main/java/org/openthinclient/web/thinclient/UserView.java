4f1094b3d6797f9874cb2737db5569589d73e14d
Marc Häfner <m.haefner@openthinclient.com>
2019-08-02 19:29:52 +0200

    Fix / Add missing create action for User items

--- console-web/webapp/src/main/java/org/openthinclient/web/thinclient/UserView.java
+++ console-web/webapp/src/main/java/org/openthinclient/web/thinclient/UserView.java
@@ -359,6 +359,19 @@ public final class UserView extends AbstractThinclientView {
   }
 
   @Override
+  public void enter(ViewChangeListener.ViewChangeEvent event) {
+    LOGGER.debug("enter -> source={}, navigator-state=", event.getSource(), event.getNavigator().getState());
+    String[] params = Optional.ofNullable(event.getParameters()).orElse("").split("/");
+    if (params.length > 0) {
+      if ("create".equals(params[0])) {
+        showProfileMetadata(new User());
+      } else {
+        super.enter(event);
+      }
+    }
+  }
+
+  @Override
   public void showOverview() {
     super.showOverview();
     overviewCL.addComponent(
