9778e05b0c2ae2b257ca0e954907d1b5e556a49e
Marc Häfner <m.haefner@openthinclient.com>
2020-07-20 17:21:50 +0200

    Fix / improve manager update checker
    * Use install4j version comparison instead of (not entirely correctly
      implemented) Version from package manager.
    * Update install4j lib so we can use `isVersionGreaterThan`

--- services/update/src/main/java/org/openthinclient/service/update/UpdateChecker.java
+++ services/update/src/main/java/org/openthinclient/service/update/UpdateChecker.java
@@ -1,10 +1,11 @@
 package org.openthinclient.service.update;
 
 import org.openthinclient.manager.util.http.DownloadManager;
-import org.openthinclient.pkgmgr.db.Version;
 import org.openthinclient.progress.NoopProgressReceiver;
 import org.openthinclient.progress.ProgressReceiver;
 import org.openthinclient.manager.util.http.DownloadException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
@@ -14,6 +15,8 @@ import java.util.*;
 import javax.annotation.PostConstruct;
 
 public class UpdateChecker {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(UpdateChecker.class);
 
   @Autowired
   private ApplicationContext applicationContext;
@@ -28,7 +31,6 @@ public class UpdateChecker {
 
   private AvailableVersionChecker versionChecker;
   private ProgressReceiver noopProgressReceiver = new NoopProgressReceiver();
-  private Version currentVersion;
   private Optional<String> newVersion = Optional.empty();
   private boolean hasNetworkError = false;
 
@@ -37,7 +39,6 @@ public class UpdateChecker {
   @PostConstruct
   public void init() {
     versionChecker = new AvailableVersionChecker(downloadManager);
-    currentVersion = Version.parse(applicationVersionString);
   }
 
   public Optional<String> getNewVersion() {
@@ -59,15 +60,19 @@ public class UpdateChecker {
       try {
         UpdateDescriptor updateDescriptor = versionChecker.getVersion(new URI(updateLocation), noopProgressReceiver);
         String newVersionString = updateDescriptor.getNewVersion();
-        int result = currentVersion.compareTo(Version.parse(newVersionString));
-        if (result < 0) {
+        if (com.install4j.api.update.UpdateChecker.isVersionGreaterThan(newVersionString, applicationVersionString)) {
           newVersion = Optional.of(newVersionString);
         } else {
           newVersion = Optional.empty();
         }
         this.hasNetworkError = false;
+      } catch(DownloadException ex) {
+        this.hasNetworkError = true;
+        LOGGER.warn("Update check failed due to network error");
+        failure = true;
       } catch (Exception ex) {
-        this.hasNetworkError = ex instanceof DownloadException;
+        this.hasNetworkError = false;
+        LOGGER.error("Update check failed", ex);
         failure = true;
       }
       isRunning = false;
