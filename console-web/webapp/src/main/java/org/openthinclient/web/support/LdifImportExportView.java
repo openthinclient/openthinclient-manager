c513f9fafcd651ec0983fbf29df84427883afd97
Marc Häfner <m.haefner@openthinclient.com>
2021-06-04 15:50:49 +0200

    Fix and improve LDIF export / import
    * Fix: Don't blindly write LDIF file with user-provided name directly
      into manager home, possibly overwriting required files or failing on
      directories. Use a temporary file instead.
    * Fix: Remove uploaded file after import.
    * Fix: Reset feedback from last run before attempting new import /
      export, don't report error if everything is OK.
    * Fix / workaround: Explicitly push feedback to UI.
    * Overhaul view
      - Rearrange elements. Move export to top, above import.
      - Put information into info texts.
      - Style fix for failure feedback. Reuse Vaadin CSS class (analogous to
        success feedback).
      - Replace unwieldy layouts with CssLayouts and CSS.
    * Improve code readability
      - Replace multi-functional, statefull inner class with lambdas. Move
        state to outer class.
      - Declare private function as private.
      - Remove unnecessary interface and @Overwrites.
      - Reduce code repetition.
      - Remove unhelpful and misleading comments.

--- console-web/webapp/src/main/java/org/openthinclient/web/support/LdifImportExportView.java
+++ console-web/webapp/src/main/java/org/openthinclient/web/support/LdifImportExportView.java
@@ -2,27 +2,24 @@ package org.openthinclient.web.support;
 
 import ch.qos.cal10n.MessageConveyor;
 import com.vaadin.navigator.View;
-import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.FileDownloader;
 import com.vaadin.server.Page;
 import com.vaadin.server.StreamResource;
 import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.Upload;
-import com.vaadin.ui.VerticalLayout;
 import org.openthinclient.api.ldif.export.LdifExporterService;
 import org.openthinclient.api.ldif.export.LdifImporterService;
 import org.openthinclient.common.model.service.RealmService;
 import org.openthinclient.service.common.home.ManagerHome;
 import org.openthinclient.web.event.DashboardEvent;
 import org.openthinclient.web.filebrowser.FileUploadSubWindow;
-import org.openthinclient.web.filebrowser.FileUploadView;
 import org.openthinclient.web.i18n.ConsoleWebMessages;
 import org.openthinclient.web.ui.ManagerSideBarSections;
 import org.openthinclient.web.ui.SettingsUI;
@@ -34,18 +31,25 @@ import org.vaadin.spring.sidebar.annotation.SideBarItem;
 
 import javax.annotation.PostConstruct;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.file.Path;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_LDIF_EXPORT_DESCRIPTION;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_LDIF_EXPORT_FAILED;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_LDIF_EXPORT_SUCCESS;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_LDIF_IMPORT_DESCRIPTION;
 import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_LDIF_IMPORT_EXPORT_HEADER;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_LDIF_IMPORT_FAILED;
+import static org.openthinclient.web.i18n.ConsoleWebMessages.UI_SUPPORT_LDIF_IMPORT_SUCCESS;
 
 @SpringView(name = "ldif-import-export", ui = SettingsUI.class)
 @SideBarItem(sectionId = ManagerSideBarSections.SERVER_MANAGEMENT, captionCode = "UI_SUPPORT_LDIF_IMPORT_EXPORT_HEADER", order = 71)
-public class LdifImportExportView extends Panel implements View, FileUploadView {
+public class LdifImportExportView extends Panel implements View {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(LdifImportExportView.class);
 
@@ -55,17 +59,15 @@ public class LdifImportExportView extends Panel implements View, FileUploadView 
   private RealmService realmService;
 
   private EventBus.SessionEventBus eventBus;
-  private MyReceiver receiver = new MyReceiver();
   final MessageConveyor mc;
-  final VerticalLayout root;
+  final CssLayout root;
+
+  private File file;
 
   private Label importSuccessLabel;
   private Label importErrorLabel;
   private Label exportErrorLabel;
   private Label exportSuccessLabel;
-
-  Set<LdifImporterService.State> importResult = new HashSet<>();
-  Set<LdifExporterService.State> exportResult = new HashSet<>();
 
   public LdifImportExportView(EventBus.SessionEventBus eventBus) {
 
@@ -74,9 +76,7 @@ public class LdifImportExportView extends Panel implements View, FileUploadView 
     mc = new MessageConveyor(UI.getCurrent().getLocale());
     setSizeFull();
 
-    root = new VerticalLayout();
-    root.setSizeFull();
-    root.setMargin(true);
+    root = new CssLayout();
     root.setStyleName("importexport");
     setContent(root);
   }
@@ -93,63 +93,78 @@ public class LdifImportExportView extends Panel implements View, FileUploadView 
 
   private void buildContent() {
 
-    VerticalLayout content = new VerticalLayout();
+    // Export
+    exportSuccessLabel = buildFeedbackLabel(UI_SUPPORT_LDIF_EXPORT_SUCCESS, "success");
+    exportErrorLabel = buildFeedbackLabel(UI_SUPPORT_LDIF_EXPORT_FAILED, "failure");
+    root.addComponent(new CssLayout(
+      new CssLayout(
+        buildExportButton(),
+        exportSuccessLabel,
+        exportErrorLabel
+      ),
+      new Label(mc.getMessage(UI_SUPPORT_LDIF_EXPORT_DESCRIPTION), ContentMode.HTML)
+    ));
 
     // Import
-    final Label labelImportDescription = new Label(mc.getMessage(ConsoleWebMessages.UI_SUPPORT_LDIF_IMPORT_DESCRIPTION), ContentMode.HTML);
-    content.addComponent(labelImportDescription);
-    HorizontalLayout importHL = new HorizontalLayout();
-    content.addComponent(importHL);
-    importSuccessLabel = new Label(mc.getMessage(ConsoleWebMessages.UI_SUPPORT_LDIF_IMPORT_SUCCESS), ContentMode.HTML);
-    importSuccessLabel.setVisible(false);
-    importErrorLabel = new Label(mc.getMessage(ConsoleWebMessages.UI_SUPPORT_LDIF_IMPORT_FAILED), ContentMode.HTML);
-    importErrorLabel.setVisible(false);
-    importErrorLabel.setStyleName("unexpected_error");
+    importSuccessLabel = buildFeedbackLabel(UI_SUPPORT_LDIF_IMPORT_SUCCESS, "success");
+    importErrorLabel = buildFeedbackLabel(UI_SUPPORT_LDIF_IMPORT_FAILED, "failure");
+    root.addComponent(new CssLayout(
+      new CssLayout(
+        buildUploadButton(),
+        importSuccessLabel,
+        importErrorLabel
+      ),
+      new Label(mc.getMessage(UI_SUPPORT_LDIF_IMPORT_DESCRIPTION), ContentMode.HTML)
+    ));
 
-    Upload upload = new Upload(null, receiver);
+  }
+
+  private Label buildFeedbackLabel(ConsoleWebMessages msg, String... styleNames) {
+    Label label = new Label(mc.getMessage(msg), ContentMode.HTML);
+    label.setVisible(false);
+    label.addStyleNames(styleNames);
+    return label;
+  }
+
+  private Button buildExportButton() {
+    Button exportButton = new Button("LDIF Export");
+    exportButton.addStyleName("ldif-export");
+    FileDownloader fileDownloader = new FileDownloader(createDownloadResource());
+    fileDownloader.extend(exportButton);
+    return exportButton;
+  }
+
+  private Upload buildUploadButton() {
+    Upload upload = new Upload(null, (filename, mimeType) -> {
+      try {
+        file = File.createTempFile(filename, "ldif", managerHome.getLocation());
+        return new FileOutputStream(file);
+      } catch (final IOException e) {
+        LOGGER.error("Could not open file", e);
+        return null;
+      }
+    });
     upload.addStyleName("ldif-import");
     upload.setButtonCaption("LDIF Import");
     upload.setAcceptMimeTypes("text/ldif");
-    upload.addSucceededListener(receiver);
+    upload.addSucceededListener(ev -> this.importLdifFile(file));
+    upload.addFailedListener(ev -> this.uploadFailed(ev.getReason()));
     upload.setImmediateMode(true);
     upload.addStartedListener(e -> {
       importSuccessLabel.setVisible(false);
       importErrorLabel.setVisible(false);
+      getUI().access(() -> getUI().push());
     });
-    importHL.addComponents(upload, importSuccessLabel, importErrorLabel);
-
-    content.addComponent(new Label("<hr/>",ContentMode.HTML));
-
-    // Export
-    final Label labelExportDescription = new Label(mc.getMessage(ConsoleWebMessages.UI_SUPPORT_LDIF_EXPORT_DESCRIPTION), ContentMode.HTML);
-    content.addComponent(labelExportDescription);
-
-    HorizontalLayout exportHL = new HorizontalLayout();
-    content.addComponent(exportHL);
-    exportSuccessLabel = new Label(mc.getMessage(ConsoleWebMessages.UI_SUPPORT_LDIF_EXPORT_SUCCESS), ContentMode.HTML);
-    exportSuccessLabel.setVisible(false);
-    exportErrorLabel = new Label(mc.getMessage(ConsoleWebMessages.UI_SUPPORT_LDIF_EXPORT_FAILED), ContentMode.HTML);
-    exportErrorLabel.setVisible(false);
-    exportErrorLabel.setStyleName("unexpected_error");
-    Button exportButton = new Button("LDIF Export");
-    exportButton.addStyleName("ldif-export");
-    // attach file-downloader
-    FileDownloader fileDownloader = new FileDownloader(createResource());
-    fileDownloader.extend(exportButton);
-    exportHL.addComponents(exportButton, exportSuccessLabel, exportErrorLabel);
-
-
-    root.addComponent(content);
-    root.setExpandRatio(content, 1);
+    return upload;
   }
 
-
-  private StreamResource createResource() {
+  private StreamResource createDownloadResource() {
     return new StreamResource((StreamResource.StreamSource) () -> {
       LdifExporterService ldifExporterService = new LdifExporterService(realmService.getDefaultRealm().getConnectionDescriptor());
+      Set<LdifExporterService.State> exportResult = new HashSet<>();
       byte[] bytes = ldifExporterService.performAction(Collections.singleton(""), exportResult::add);
       if (exportResult.contains(LdifExporterService.State.ERROR) || exportResult.contains(LdifExporterService.State.EXCEPTION)) {
-        exportErrorLabel.setVisible(true); // will be shown only on a UI-click (..somewhere)
+        exportErrorLabel.setVisible(true);
         return null;
       } else {
         exportSuccessLabel.setVisible(true);
@@ -158,70 +173,27 @@ public class LdifImportExportView extends Panel implements View, FileUploadView 
     }, "openthinclient-export.ldif");
   }
 
-  public void uploadSucceed(Path file) {
+  private void importLdifFile(File file) {
     LdifImporterService lis = new LdifImporterService(realmService.getDefaultRealm().getConnectionDescriptor());
+    Set<LdifImporterService.State> importResult = new HashSet<>();
     try {
-      lis.importTempFile(file.toFile(), importResult::add);
-      // check import-result
+      lis.importTempFile(file, importResult::add);
       if (importResult.contains(LdifImporterService.State.ERROR) || importResult.contains(LdifImporterService.State.EXCEPTION)) {
         importErrorLabel.setVisible(true);
       } else {
         importSuccessLabel.setVisible(true);
       }
     } catch (Exception e) {
-      LOGGER.error("Failed to import file " + file.getFileName(), e);
+      LOGGER.error("Failed to import file " + file.getName(), e);
       importErrorLabel.setVisible(true);
     }
     eventBus.publish(this, new DashboardEvent.ClientCountChangeEvent());
+    file.delete();
   }
 
-  @Override
-  public void uploadFailed(Exception exception) {
+  private void uploadFailed(Exception exception) {
     importErrorLabel.setVisible(true);
-    LOGGER.error("Failed to import ldif-file.", exception);
-  }
-
-  @Override
-  public void enter(ViewChangeListener.ViewChangeEvent event) {
-
-  }
-
-  /**
-   * The file upload receiver.
-   */
-  public class MyReceiver implements Upload.Receiver, Upload.SucceededListener, Upload.FailedListener {
-
-    /** serialVersionUID */
-    private static final long serialVersionUID = -5844542658116931976L;
-    private final transient Logger LOGGER = LoggerFactory.getLogger(FileUploadSubWindow.MyReceiver.class);
-
-    public Path file;
-
-    public OutputStream receiveUpload(String filename, String mimeType) {
-      // Create upload stream
-      FileOutputStream fos = null; // Stream to write to
-      try {
-        // Open the file for writing.
-        // TODO path
-        file = managerHome.getLocation().toPath().resolve(filename);
-        fos = new FileOutputStream(file.toFile());
-      } catch (final java.io.FileNotFoundException e) {
-        LOGGER.error("Could not open file", e);
-        new Notification(mc.getMessage(ConsoleWebMessages.UI_FILEBROWSER_SUBWINDOW_UPLOAD_FAIL), Notification.Type.ERROR_MESSAGE).show(Page.getCurrent());
-        return null;
-      }
-      return fos; // Return the output stream to write to
-    }
-
-    @Override
-    public void uploadSucceeded(Upload.SucceededEvent event) {
-      LdifImportExportView.this.uploadSucceed(file);
-    }
-
-    @Override
-    public void uploadFailed(Upload.FailedEvent failedEvent) {
-      // TODO show Error
-    }
+    LOGGER.error("Failed to import LDIF-file.", exception);
   }
 
 }
