0e56d243e69d5c5a95ce04bc4dae2a7dd04bcc78
Marc Häfner <m.haefner@openthinclient.com>
2021-07-19 17:09:24 +0200

    Minor cleanup: Remove unused method

--- services/tftp/src/main/java/org/openthinclient/tftp/PXEConfigTFTProvider.java
+++ services/tftp/src/main/java/org/openthinclient/tftp/PXEConfigTFTProvider.java
@@ -307,15 +307,4 @@ public class PXEConfigTFTProvider implements TFTPProvider {
     }
     return null;
   }
-
-  private String streamAsString(InputStream is) throws IOException {
-    final ByteArrayOutputStream s = new ByteArrayOutputStream();
-    final byte b[] = new byte[1024];
-    int read;
-    while ((read = is.read(b)) >= 0)
-      s.write(b, 0, read);
-
-    is.close();
-    return s.toString("ASCII");
-  }
 }
