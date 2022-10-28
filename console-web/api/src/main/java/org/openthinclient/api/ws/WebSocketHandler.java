f420b47d7979c0ff49f3ad453ae3e066d5a5a207
Marc Häfner <m.haefner@openthinclient.com>
2022-10-28 15:10:37 +0200

    Fix websocket threading/deadlock issues
    * Replace synchronizing over `sessions` Set with a "ConcurentHashSet"
      (KeySet from ConcurrentHashMap)
    * Use Spring's ConcurrentWebSocketSessionDecorator to handle concurreny
      and stale / broken sessions

--- console-web/api/src/main/java/org/openthinclient/api/ws/WebSocketHandler.java
+++ console-web/api/src/main/java/org/openthinclient/api/ws/WebSocketHandler.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.ConcurrentWebSocketSessionDecorator;
+import org.springframework.web.socket.handler.SessionLimitExceededException;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -18,26 +19,24 @@ import java.util.function.BiConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.springframework.web.socket.handler.ConcurrentWebSocketSessionDecorator.OverflowStrategy.TERMINATE;
+
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
     private static final Logger LOG = LoggerFactory.getLogger(WebSocketHandler.class);
 
-    private static Set<WebSocketSession> sessions = new HashSet<>();
+    private static Set<WebSocketSession> sessions = ConcurrentHashMap.newKeySet();
     private static Map<String, List<BiConsumer<WebSocketSession, String>>> consumers = new ConcurrentHashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        synchronized(sessions) {
-            sessions.add(session);
-        }
+        sessions.add(session);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         super.afterConnectionClosed(session, status);
-        synchronized(sessions) {
-            sessions.remove(session);
-        }
+        sessions.remove(session);
     }
 
     public void register(String message_type, BiConsumer<WebSocketSession, String> consumer) {
@@ -68,13 +67,15 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
 
 	public void sendToAll(String message_type) {
-        synchronized(sessions) {
-            for(WebSocketSession session: sessions) {
-                try {
-                    session.sendMessage(new TextMessage(message_type));
-                } catch(IOException ex) {
-                    LOG.error("Failed to send {} to {}", message_type, session.getRemoteAddress());
-                }
+        for(WebSocketSession session: sessions) {
+            try (WebSocketSession wrappedSession = new ConcurrentWebSocketSessionDecorator(
+                    session, 1000 /*ms*/, 1024/*B*/, TERMINATE)) {
+                wrappedSession.sendMessage(new TextMessage(message_type));
+            } catch(IOException ex) {
+                LOG.error("Failed to send {} to {}", message_type, session.getRemoteAddress());
+            } catch(SessionLimitExceededException ex) {
+                LOG.info("Could not send {} to {}. Seems to be offline.", message_type, session.getRemoteAddress());
+                sessions.remove(session);
             }
         }
 	}
