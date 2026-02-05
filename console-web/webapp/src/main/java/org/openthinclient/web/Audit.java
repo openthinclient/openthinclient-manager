9080780aac1430a8e5d5ebf123673c4da57204a4
Marc Häfner <m.haefner@openthinclient.com>
2026-02-05 12:20:26 +0100

    Add user name to audit log

--- console-web/webapp/src/main/java/org/openthinclient/web/Audit.java
+++ console-web/webapp/src/main/java/org/openthinclient/web/Audit.java
@@ -1,9 +1,12 @@
 package org.openthinclient.web;
 
+import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.*;
 import org.openthinclient.common.model.DirectoryObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.*;
 import java.util.stream.*;
@@ -12,11 +15,22 @@ public class Audit {
   private static final Logger LOGGER = LoggerFactory.getLogger("Audit");
 
   private static void log(String op, String type, String... details) {
+    String name="<unknown>";
+    try {
+      SecurityContext securityContext = (SecurityContext) VaadinSession
+          .getCurrent().getSession()
+          .getAttribute("SPRING_SECURITY_CONTEXT");
+      UserDetails userDetails = (UserDetails) securityContext
+          .getAuthentication().getPrincipal();
+      name = userDetails.getUsername();
+    } catch (Exception ex) {
+      LOGGER.warn("Failed to get user name for audit log", ex);
+    }
     if(details == null) {
       details = new String[]{};
     }
     String ip = UI.getCurrent().getPage().getWebBrowser().getAddress();
-    LOGGER.info(Stream.concat(Stream.of(ip, op, type),
+    LOGGER.info(Stream.concat(Stream.of(ip, name, op, type),
                               Arrays.stream(details))
                       .map(str -> Objects.isNull(str)? "": str)
                       .collect(Collectors.joining("\t")));
